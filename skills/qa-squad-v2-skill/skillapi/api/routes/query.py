c2760131482be5b5aaeb12769b5db4c3c96ccdf1
Tim Baumgärtner <baumgaertner.t@gmail.com>
2021-10-25 11:22:24 +0200

    squad-v2 skill wip

---
+++ skills/qa-squad-v2-skill/skillapi/api/routes/query.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+
+from skillapi.models.request import QueryRequest
+from skillapi.models.prediction import QueryOutput
+from skillapi.skill import predict
+import logging
+
+logger = logging.getLogger(__name__)
+
+router = APIRouter()
+
+
+@router.post("/query", response_model=QueryOutput, name="Skill Query")
+async def query(query: QueryRequest) -> QueryOutput:
+    logger.info(f"Query: {query.dict()}")
+    prediction = await predict(query)
+
+    return prediction
