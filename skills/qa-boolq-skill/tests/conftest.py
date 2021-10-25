44c2d2fc6634a696b952d500c436944d418f80ea
Tim Baumgärtner <baumgaertner.t@gmail.com>
2021-10-25 11:49:46 +0200

    tests

---
+++ skills/qa-boolq-skill/tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+import json
+from copy import deepcopy
+
+from starlette.config import environ
+MODEL_API_KEY = "test_key"
+MODEL_API_URL = "http://localhost:8080/api/<model_name>/<endpoint>"
+DATA_API_URL = "http://localhost:8080/datastore/<datastore_name>/indexs/<index_name>/search"
+environ["MODEL_API_KEY"] = MODEL_API_KEY
+environ["MODEL_API_URL"] = MODEL_API_URL
+environ["DATA_API_URL"] = DATA_API_URL
+
+from main import app
+
+model_api_response_json = json.load(open("tests/model_api_question_answering_response.json"))
+
+@pytest.fixture()
+def test_app():
+    return app
+
+@pytest.fixture()
+def model_api_response():
+    return deepcopy(model_api_response_json)
+
---
+++ skills/qa-boolq-skill/tests/test_query/test_query_predict.py
@@ -0,0 +1,51 @@
+import pytest
+
+from skillapi.models.request import QueryRequest
+from skillapi.skill.skill import decode_model_api_response, call_model_api, predict
+import numpy as np
+
+class Response:
+    def __init__(self, status_code, json={}, text=""):
+        self._json = json
+        self.status_code = status_code
+        self.text = text
+
+    def json(self):
+        return self._json
+
+
+def mock_request(response_json, status_code=200, error_text=""):
+    def f(url, json={}, params={}, headers={}):
+        return Response(status_code, json=response_json, text=error_text)
+    return f
+
+
+def test_decode_model_api_response_encoded(model_api_response):
+    model_api_response = decode_model_api_response(model_api_response)
+    assert isinstance(model_api_response["model_outputs"]["logits"], np.ndarray)
+
+
+def test_decode_model_api_response_list(model_api_response):
+    model_api_response["model_output_is_encoded"] = False
+    model_api_response["model_outputs"]["logits"] = [[1, 0.5, 4.2]]
+    model_api_response = decode_model_api_response(model_api_response)
+    assert isinstance(model_api_response["model_outputs"]["logits"], np.ndarray)
+
+
+@pytest.mark.asyncio
+async def test_model_api_call(mocker, model_api_response):
+    mocker.patch("skillapi.skill.skill.requests.post", mock_request(model_api_response, 200))
+    res = await call_model_api("url", {})
+
+
+@pytest.mark.asyncio
+async def test_model_api_call_error(mocker):
+    mocker.patch("skillapi.skill.skill.requests.post", mock_request({}, 500, "Error"))
+    with pytest.raises(RuntimeError):
+        res = await call_model_api("url", {})
+
+@pytest.mark.asyncio
+async def test_predict(mocker, model_api_response) -> None:
+    mocker.patch("skillapi.skill.skill.requests.post", mock_request(model_api_response, 200))
+    res = await predict(QueryRequest(**{"query": "The query", "num_results": 1, "skill_args": {}, "user_id": "1234"}))
+    pass
