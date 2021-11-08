99c4a7376192575e86a8ff7f1b736322e1d237ba
Max Eichler <maexe@users.noreply.github.com>
2021-11-08 16:57:23 +0100

    Fix my skills link

--- square-frontend/src/components/NavBar.vue
+++ square-frontend/src/components/NavBar.vue
@@ -29,7 +29,7 @@
                 {{ user.name }}
               </a>
               <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-                <li><a class="dropdown-item" href="/skills">My skills</a></li>
+                <li><router-link class="dropdown-item" to="/skills">My skills</router-link></li>
                 <li><hr class="dropdown-divider"></li>
                 <li><a class="dropdown-item" v-on:click.prevent="signout" href="#">Sign out</a></li>
               </ul>
