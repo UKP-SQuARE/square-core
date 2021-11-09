9707979f8962701f2a27cafccc5a08d58bab6f8a
Tim Baumgärtner <baumgaertner.t@gmail.com>
2021-11-09 09:59:03 +0100

    use skill-api and skill-helpers package

--- skills/reference-skill-example/skillapi/skill/skill.py
+++ skills/reference-skill-example/skillapi/skill/skill.py
@@ -1,80 +1,18 @@
-import base64
+import logging
 import uuid
-from io import BytesIO
-from typing import Iterable
 
-import requests
-import numpy as np
+from square_skill_api.models.prediction import QueryOutput
+from square_skill_api.models.request import QueryRequest
 
-from skillapi.core.config import DATA_API_URL, MODEL_API_URL, MODEL_API_KEY
-from skillapi.models.prediction import QueryOutput
-from skillapi.models.request import QueryRequest
+from square_skill_helpers.config import SquareSkillHelpersConfig
+from square_skill_helpers.square_api import ModelAPI, DataAPI
 
+logger = logging.getLogger(__name__)
 
-def decode_model_api_response(model_api_response):
-    """
-    Decode (if necessary) the model output of the Model API response and make it into
-    np arrays.
-    :param model_api_response: The response from the API
-    :return: model_api_response with 'model_outputs' decoded and parsed to numpy
-    """
-    # Decode byte base64 string back to numpy array
-    def _decode(arr_string_b64):
-        arr_binary_b64 = arr_string_b64.encode()
-        arr_binary = base64.decodebytes(arr_binary_b64)
-        arr = np.load(BytesIO(arr_binary))
-        return arr
-    # Recursively go through a value and decodeleaves (=str) or iterate over values and decode them
-    def dec_or_iterate(val):
-        if isinstance(val, str):
-            return _decode(val)
-        elif isinstance(val, Iterable):
-            return [dec_or_iterate(v) for v in val]
-        else:
-            raise ValueError(f"Encountered unexpected value {type(val)} while trying to decode the model output of the model API. "
-                             f"Expected str or iterable.")
-    if model_api_response["model_output_is_encoded"]:
-        model_api_response["model_outputs"] = {key: dec_or_iterate(arr) for key, arr in model_api_response["model_outputs"].items()}
-    else:
-        model_api_response["model_outputs"] = {key: np.array(arr) for key, arr in model_api_response["model_outputs"].items()}
-    return model_api_response
+config = SquareSkillHelpersConfig.from_dotenv()
+model_api = ModelAPI(config)
+data_api = DataAPI(config)
 
-
-async def call_model_api(url, model_request):
-    """
-    Call the Model API with the given complete URL (http://<host>:<port>/api/<model_name>/<endpoint>) and request.
-    The 'model_outputs' will be decoded if necessary and made into np arrays before returning.
-    :param url: The complete URL to send the request to
-    :param model_request: the request to use for the call
-    :return: The response from the Model API. If the request was not succesfull, an exception is raised.
-    """
-    response = requests.post(url, json=model_request, headers={"Authorization": MODEL_API_KEY})
-    if response.status_code == 200:
-        return decode_model_api_response(response.json())
-    else:
-        raise RuntimeError(f"Request to model API at URL {url} with request {model_request} "
-                           f"failed with code {response.status_code} and message {response.text}")
-
-
-async def call_data_api(url, data_request):
-    """
-    Call the Data API with the given complete URL (http://<host>:<port>/datastore/<datastore_name>/indexs/<index_name>/search) and request.
-    The 'model_outputs' will be decoded if necessary and made into np arrays before returning.
-    :param url: The complete URL to send the request to
-    :param model_request: the request to use for the call
-    :return: The response from the Data API. If the request was not succesfull, an exception is raised.
-    """
-    response = requests.get(url, params=data_request)
-    if response.status_code == 200:
-        return response.json()["root"]["children"]
-    else:
-        raise RuntimeError(f"Request to data API at URL {url} with request {data_request} "
-                           f"failed with code {response.status_code} and message {response.text}")
-
-
-# Example prediction function for question answering.
-# We first retrieve documents with die Data API
-# Then we use the Model API to perform span extraction
 async def predict(request: QueryRequest) -> QueryOutput:
     """
     Process a given query and create the predictions for it.
@@ -86,7 +24,7 @@ async def predict(request: QueryRequest) -> QueryOutput:
         "query": request.query,
         "top_k": request.num_results
     }
-    data = await call_data_api(DATA_API_URL, data_request)
+    data = await DataAPI(datastore="wiki", index_name="dpr", data_request=data_request)
 
     # Call Model API
     prepared_input = [[request.query, d["fields"]["text"]] for d in data] # Change as needed
@@ -100,11 +38,16 @@ async def predict(request: QueryRequest) -> QueryOutput:
 
     # Format depends on your task/ endpoint - check out the Model API
     # or square-core/square-model-inference-api/inference_server/square_model_inference/models/prediction.py
-    output = await call_model_api(MODEL_API_URL, model_request)
+    output = await model_api(
+        model_name="bert-base-uncased", 
+        pipline="question-answering", 
+        model_request=model_request
+    )
+    logger.info(f"Model API output:\n{output}")
 
     # Prepare prediction
     query_output = []
-    index_name = DATA_API_URL.split("/")[-2]
+    index_name = "wiki/dpr"
     for d, ans in zip(data, output["answers"]):
         ans = ans[0]
         if not ans["answer"]:
