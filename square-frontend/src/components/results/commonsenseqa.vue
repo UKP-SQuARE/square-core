755727eb22e697ced3bb7668d5ed6a2694435a79
Max Eichler <maexe@users.noreply.github.com>
2021-11-12 09:40:11 +0100

    Add CommonsenseQA result component

---
+++ square-frontend/src/components/results/commonsenseqa.vue
@@ -0,0 +1,33 @@
+<!-- Component for the Results. The user can see the results of each chosen skill here. Results can have different formats. -->
+<template>
+  <ul class="list-group list-group-flush">
+    <li
+        v-for="(res, index) in skillResult.results"
+        :key="index"
+        class="list-group-item">
+      <div class="d-flex w-100 justify-content-between">
+        <div>{{ index + 1 }}. <span class="fw-bold">{{ res.prediction_output.output }}</span></div>
+        <span class="badge bg-primary p-2">{{ roundScore(res.prediction_score) }}%</span>
+      </div>
+      <div class="progress mt-2">
+        <div
+            class="progress-bar progress-bar-striped bg-primary"
+            role="progressbar"
+            :style="{ width: `${roundScore(res.prediction_score)}%` }"
+            :aria-valuenow="roundScore(res.prediction_score)"
+            aria-valuemin="0"
+            aria-valuemax="100" />
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+import Vue from 'vue'
+import mixin from '@/components/results/mixin.vue'
+
+export default Vue.component('commonsenseqa', {
+  props: ['skillResult'],
+  mixins: [mixin]
+})
+</script>
