5ab272bf4ac54826e6975f35e68f875b2f7e6be7
Max Eichler <maexe@users.noreply.github.com>
2021-11-12 09:46:03 +0100

    Fix navbar layout

--- square-frontend/src/components/NavBar.vue
+++ square-frontend/src/components/NavBar.vue
@@ -2,14 +2,14 @@
 <template>
   <header class="p-2 bg-primary">
     <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
         <div class="container-fluid">
           <router-link class="navbar-brand" to="/">SQuARE</router-link>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon" />
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto">
               <li class="nav-item">
                 <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
               </li>
