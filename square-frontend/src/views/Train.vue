2e0b08278a531297649083c7e2b5a47a2793dce2
Max Eichler <maexe@users.noreply.github.com>
2021-11-15 15:19:27 +0100

    Remove BootstrapVue from train view

--- square-frontend/src/views/Train.vue
+++ square-frontend/src/views/Train.vue
@@ -1,153 +1,166 @@
 <!-- The Page of a Skill. The user can edit an existing skill or create a new skill here. -->
 <template>
-  <b-container>
-    <h2 class="text-center">
-      {{skill.name}}
-      <b-badge variant="info" v-if="skill.is_published" class="ml-1 mb-1">Published</b-badge>
-      <b-badge variant="secondary" v-else class="ml-1 mb-1">Not Published</b-badge>
-    </h2>
-    <hr />
-
-    <b-alert v-model="success" variant="success" dismissible>{{successMessage}}</b-alert>
-    <b-alert v-model="failure" variant="danger" dismissible>There was a problem: {{failureMessage}}</b-alert>
-
-    <b-button to="/skills" variant="outline-secondary" class="float-left">◁ Back to My Skills</b-button>
-
-    <b-form class="offset-md-2 col-md-8" @submit.prevent="trainSkill">
-      <b-form-group
-        description="Text file containing example questions. Each line is treated as one example. File is expected to be UFT-8-encoded and smaller than 5MB."
-      >
-        <label
-          for="train_file"
-        >{{skill.is_published ? 'Upload training data (this will overwrite previous training data and retrain models):' : 'Upload training data:'}}</label>
-        <b-form-file
-          v-model="train_file"
-          :state="Boolean(train_file)"
-          name="train_file"
-          placeholder="Choose a file or drop it here..."
-          drop-placeholder="Drop file here..."
-        ></b-form-file>
-      </b-form-group>
-
-      <b-form-group
-        description="Text file containing example dev questions for validation. Each line is treated as one example. File is expected to be UFT-8-encoded and smaller than 5MB."
-      >
-        <label
-          for="dev_file"
-        >{{skill.is_published ? 'Upload dev data (this will overwrite previous dev data and retrain models):' : 'Upload dev data:'}}</label>
-        <b-form-file
-          v-model="dev_file"
-          :state="Boolean(dev_file)"
-          name="dev_file"
-          placeholder="Choose a file or drop it here..."
-          drop-placeholder="Drop file here..."
-        ></b-form-file>
-      </b-form-group>
-
-      <b-button
-        v-if="!waitingTraining"
-        type="submit"
-        variant="outline-primary"
-        class="mr-2"
-      >{{skill.is_published ? 'Retrain' : 'Train and Publish'}}</b-button>
-      <b-button v-else type="submit" variant="outline-primary" class="mr-2" disabled>
-        Training...
-        <b-spinner small label="Spinning"></b-spinner>
-      </b-button>
-
-      <b-button
-        v-if="skill.is_published && !waitingUnpublishing"
-        v-b-modal="'modal-'+skill.name"
-        variant="outline-danger"
-        class="float-right"
-      >Unpublish</b-button>
-      <b-button
-        v-else-if="skill.is_published && waitingUnpublishing"
-        variant="outline-danger"
-        class="float-right"
-        disabled
-      >
-        Unpublishing...
-        <b-spinner small label="Spinning"></b-spinner>
-      </b-button>
-
-      <b-modal v-bind:id="'modal-'+skill.name" title="Are you sure?">
-        <p>Please confirm that you want to unpublish {{skill.name}}.</p>
-        <p>You will have to retrain the skill if you want to publish it again.</p>
-        <template v-slot:modal-footer>
-          <b-button variant="outline-success" @click="$bvModal.hide('modal-'+skill.name)">Cancel</b-button>
-          <b-button variant="outline-danger" @click="$bvModal.hide('modal-'+skill.name); unpublishSkill()">Unpublish</b-button>
-        </template>
-      </b-modal>
-    </b-form>
-  </b-container>
+  <form v-on:submit.prevent="this.trainSkill">
+    <Card>
+      <template #leftItem>
+        <router-link to="/skills" class="btn btn-outline-primary d-inline-flex align-items-center" role="button">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-square" viewBox="0 0 16 16">
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+            <path d="M10.205 12.456A.5.5 0 0 0 10.5 12V4a.5.5 0 0 0-.832-.374l-4.5 4a.5.5 0 0 0 0 .748l4.5 4a.5.5 0 0 0 .537.082z"/>
+          </svg>
+          &nbsp;My skills
+        </router-link>
+      </template>
+      <template #topItem :skill="this.skill">
+        <h5 class="fw-light mb-0">{{ skill.name }}</h5>
+        <span v-if="skill.is_published" class="badge bg-info ms-1 p-2">Published</span>
+        <span v-else class="badge bg-secondary ms-1 p-2">Not Published</span>
+      </template>
+      <template #rightItem :waitingTraining="this.waitingTraining" :skill="this.skill">
+        <button class="btn btn-outline-primary d-inline-flex align-items-center" type="submit" :disabled="waitingTraining">
+          <span v-if="waitingTraining" class="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
+          <svg v-else xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16">
+            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+            <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/>
+          </svg>
+          &nbsp;{{ skill.is_published ? 'Overwrite' : 'Publish' }}
+        </button>
+      </template>
+      <Alert v-if="success" class="alert-success" dismissible>{{ this.successMessage }}</Alert>
+      <Alert v-if="failure" class="alert-danger" dismissible>There was a problem: {{ this.failureMessage }}</Alert>
+      <Alert v-if="this.skill.is_published" class="alert-warning">This skill has already been published. Uploading new data will overwrite and retrain the model.</Alert>
+      <div class="row mt-3">
+        <div class="col">
+          <label for="train_file" class="form-label">Training data</label>
+          <input
+              class="form-control form-control"
+              id="train_file"
+              type="file"
+              v-on:change="handleFile($event)">
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col">
+          <label for="dev_file" class="form-label">Dev data</label>
+          <input
+              class="form-control form-control"
+              id="dev_file"
+              type="file"
+              v-on:change="handleFile($event)">
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col">
+          Text files are expected to contain example questions seperated by line breaks. All file should be UTF-8 encoded and &lt; 5MB.
+        </div>
+      </div>
+      <div v-if='this.skill.is_published || this.waitingUnpublishing' class="row mt-3">
+        <div class="col">
+          <button
+              class="btn btn-outline-danger d-inline-flex align-items-center"
+              data-bs-toggle="modal"
+              data-bs-target="#unpublishModal"
+              :disabled="this.waitingUnpublishing">
+            <span v-if="this.waitingUnpublishing" class="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
+            &nbsp;Unpublish
+          </button>
+          <div class="modal fade" id="unpublishModal" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Are you sure?</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  Please confirm that you want to unpublish <strong>{{ this.skill.name }}</strong>.
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                  <button type="button" class="btn btn-danger" @click="this.unpublishSkill()">Unpublish</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Card>
+  </form>
 </template>
 
 
 <script>
-export default {
-  name: "train",
+import Vue from 'vue'
+import Alert from '@/components/Alert.vue'
+import Card from '@/components/Card.vue'
+
+export default Vue.component('train', {
   data() {
     return {
       train_file: null,
       dev_file: null,
       success: false,
-      successMessage: "",
+      successMessage: '',
       failure: false,
-      failureMessage: "",
+      failureMessage: '',
       waitingTraining: false,
       waitingUnpublishing: false
-    };
+    }
+  },
+  components: {
+    Alert,
+    Card
   },
   computed: {
     skill() {
-      var skills = this.$store.state.mySkills;
-      return skills.find(sk => sk.id === this.$route.params.id);
+      return this.$store.state.mySkills.find(skill => skill.id === this.$route.params.id)
     }
   },
   methods: {
     trainSkill() {
-      this.failure = false;
-      this.success = false;
-      this.waitingTraining = true;
-      this.$store.dispatch("SOCKET_train",  { id: this.skill.id, train_file: this.train_file, dev_file: this.dev_file });
+      this.failure = false
+      this.success = false
+      this.waitingTraining = true
+      this.$store.dispatch('SOCKET_train',  { id: this.skill.id, train_file: this.train_file, dev_file: this.dev_file })
     },
     unpublishSkill() {
-      this.failure = false;
-      this.success = false;
-      this.waitingUnpublishing = true;
-      this.$store.dispatch("SOCKET_unpublish", {id: this.skill.id});
+      this.failure = false
+      this.success = false
+      this.waitingUnpublishing = true
+      this.$store.dispatch('SOCKET_unpublish', { id: this.skill.id })
+    },
+    handleFile(event) {
+      this.$data[event.target.id] = event.target.files[0]
     }
   },
   sockets: {
     train(val) {
       if (val.finished) {
-        this.waitingTraining = false;
-        this.$store.dispatch("updateSkills")
-        .then(() => this.$store.commit("initQueryOptions", {forceSkillInit: true}));
+        this.waitingTraining = false
+        this.$store.dispatch('updateSkills')
+            .then(() => this.$store.commit('initQueryOptions', { forceSkillInit: true }))
       } else if (val.error) {
-        this.waitingTraining = false;
-        this.failure = true;
-        this.failureMessage = val.error;
+        this.waitingTraining = false
+        this.failure = true
+        this.failureMessage = val.error
       } else if (val.msg) {
-        this.success = true;
-        this.successMessage = val.msg;
+        this.success = true
+        this.successMessage = val.msg
       }
     },
     unpublish(val) {
       if (val.finished) {
-        this.waitingUnpublishing = false;
-        this.$store.dispatch("updateSkills")
-        .then(() => this.$store.commit("initQueryOptions", {forceSkillInit: true}));
+        this.waitingUnpublishing = false
+        this.$store.dispatch('updateSkills')
+            .then(() => this.$store.commit('initQueryOptions', { forceSkillInit: true }))
       } else if (val.error) {
-        this.waitingTraining = false;
-        this.failure = true;
-        this.failureMessage = val.error;
+        this.waitingTraining = false
+        this.failure = true
+        this.failureMessage = val.error
       } else if (val.msg) {
-        this.success = true;
-        this.successMessage = val.msg;
+        this.success = true
+        this.successMessage = val.msg
       }
     }
   }
-};
+})
 </script>
