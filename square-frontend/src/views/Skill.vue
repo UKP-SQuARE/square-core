42d4ca86f95f04fe4d25986b90e1d740798717b9
Max Eichler <maexe@users.noreply.github.com>
2021-11-15 15:18:50 +0100

    Add card component

--- square-frontend/src/views/Skill.vue
+++ square-frontend/src/views/Skill.vue
@@ -1,73 +1,62 @@
 <!-- The Page of a Skill. The user can edit an existing skill or create a new skill here. -->
 <template>
-  <div>
-    <div class="card border-primary shadow mt-3">
-      <div class="card-header">
-        <div class="row">
-          <div class="col d-flex justify-content-start">
-            <router-link to="/skills" class="btn btn-outline-primary d-inline-flex align-items-center" role="button">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-square" viewBox="0 0 16 16">
-                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                <path d="M10.205 12.456A.5.5 0 0 0 10.5 12V4a.5.5 0 0 0-.832-.374l-4.5 4a.5.5 0 0 0 0 .748l4.5 4a.5.5 0 0 0 .537.082z"/>
-              </svg>
-              &nbsp;My skills
-            </router-link>
-          </div>
-          <div class="col d-flex justify-content-center align-items-center">
-            <h5 v-if="this.originalName" class="fw-light mb-0">{{ this.originalName }}</h5>
-            <h5 v-else class="fw-light mb-0">New skill</h5>
-          </div>
-          <div class="col d-flex justify-content-end">
-            <router-link to="/skills/new_skill" class="btn btn-outline-primary d-inline-flex align-items-center" role="button">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save" viewBox="0 0 16 16">
-                <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/>
-              </svg>
-              &nbsp;Save
-            </router-link>
+  <form v-on:submit.prevent="onSubmit">
+    <Card :title="this.originalName ? this.originalName : 'New skill'">
+      <template #leftItem>
+        <router-link to="/skills" class="btn btn-outline-primary d-inline-flex align-items-center" role="button">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-square" viewBox="0 0 16 16">
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+            <path d="M10.205 12.456A.5.5 0 0 0 10.5 12V4a.5.5 0 0 0-.832-.374l-4.5 4a.5.5 0 0 0 0 .748l4.5 4a.5.5 0 0 0 .537.082z"/>
+          </svg>
+          &nbsp;My skills
+        </router-link>
+      </template>
+      <template #rightItem>
+        <router-link to="/skills/new_skill" class="btn btn-outline-primary d-inline-flex align-items-center" role="button">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save" viewBox="0 0 16 16">
+            <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/>
+          </svg>
+          &nbsp;Save
+        </router-link>
+      </template>
+      <Alert v-if="success" class="alert-success" dismissible>Skill was updated successfully.</Alert>
+      <Alert v-if="failure" class="alert-danger" dismissible>There was a problem: {{ this.failureMessage }}</Alert>
+      <div class="row mt-3">
+        <div class="col">
+          <div class="form-floating">
+            <input v-model="skill.name" type="text" class="form-control rounded-0 rounded-top" id="name" placeholder="Skill name">
+            <label for="name">Skill name</label>
           </div>
         </div>
       </div>
-      <div class="card-body">
-        <Alert v-if="success" class="alert-success" dismissible>Skill was updated successfully.</Alert>
-        <Alert v-if="failure" class="alert-danger" dismissible>There was a problem: {{ failureMessage }}</Alert>
-        <form v-on:submit.prevent="onSubmit">
-          <div class="row mt-3">
-            <div class="col">
-              <div class="form-floating">
-                <input v-model="skill.name" type="text" class="form-control rounded-0 rounded-top" id="name" placeholder="Skill name">
-                <label for="name">Skill name</label>
-              </div>
-            </div>
+      <div class="row mt-3">
+        <div class="col">
+          <div class="form-floating">
+            <input v-model="skill.description" type="text" class="form-control rounded-0 rounded-top" id="description" placeholder="Description">
+            <label for="description">Description</label>
+            <small class="text-muted">Short description of the skill</small>
           </div>
-          <div class="row mt-3">
-            <div class="col">
-              <div class="form-floating">
-                <input v-model="skill.description" type="text" class="form-control rounded-0 rounded-top" id="description" placeholder="Description">
-                <label for="description">Description</label>
-                <small class="text-muted">Short description of the skill</small>
-              </div>
-            </div>
-          </div>
-          <div class="row mt-3">
-            <div class="col">
-              <Status :url="skill.url" class="mb-2" />
-              <div class="form-floating">
-                <input v-model="skill.url" type="url" class="form-control rounded-0 rounded-top" id="url" placeholder="URL">
-                <label for="url">URL</label>
-                <small class="text-muted"><span class="text-info">scheme</span>://<span class="text-info">host</span>:<span class="text-info">port</span>/<span class="text-info">base_path</span></small>
-              </div>
-            </div>
-          </div>
-        </form>
+        </div>
       </div>
-    </div>
-  </div>
+      <div class="row mt-3">
+        <div class="col">
+          <Status :url="skill.url" class="mb-2" />
+          <div class="form-floating">
+            <input v-model="skill.url" type="url" class="form-control rounded-0 rounded-top" id="url" placeholder="URL">
+            <label for="url">URL</label>
+            <small class="text-muted"><span class="text-info">scheme</span>://<span class="text-info">host</span>:<span class="text-info">port</span>/<span class="text-info">base_path</span></small>
+          </div>
+        </div>
+      </div>
+    </Card>
+  </form>
 </template>
 
 
 <script>
 import Vue from 'vue'
 import Alert from '@/components/Alert.vue'
+import Card from '@/components/Card.vue'
 import Status from '@/components/Status.vue'
 
 export default Vue.component('skill', {
@@ -90,6 +79,7 @@ export default Vue.component('skill', {
   },
   components: {
     Alert,
+    Card,
     Status
   },
   computed: {
@@ -111,28 +101,28 @@ export default Vue.component('skill', {
     updateSkill() {
       this.success = false
       this.$store
-        .dispatch('updateSkill', { skill: this.skill })
-        .then(() => {
-          this.originalName = this.skill.name
-          this.success = true
-          this.failure = false
-        })
-        .then(() => {
-          this.$store.commit('initQueryOptions', {forceSkillInit: true})
-        })
-        .catch(failureMessage => {
-          this.failure = true
-          this.failureMessage = failureMessage
-        })
+          .dispatch('updateSkill', { skill: this.skill })
+          .then(() => {
+            this.originalName = this.skill.name
+            this.success = true
+            this.failure = false
+          })
+          .then(() => {
+            this.$store.commit('initQueryOptions', {forceSkillInit: true})
+          })
+          .catch(failureMessage => {
+            this.failure = true
+            this.failureMessage = failureMessage
+          })
     },
     createSkill() {
       this.$store
-        .dispatch('createSkill', { skill: this.skill })
-        .then(() => this.$router.push('/skills'))
-        .catch(error => {
-          this.failure = true
-          this.failureMessage = error.data.msg
-        })
+          .dispatch('createSkill', { skill: this.skill })
+          .then(() => this.$router.push('/skills'))
+          .catch(error => {
+            this.failure = true
+            this.failureMessage = error.data.msg
+          })
     }
   },
   /**
--- square-frontend/src/views/Skills.vue
+++ square-frontend/src/views/Skills.vue
@@ -1,86 +1,72 @@
 <!-- The Skills Overview Page. The user can see their skills here. They can delete, edit and train (publish) existing skills or create a new skill. -->
 <template>
-  <div>
-    <div class="card border-primary shadow mt-3">
-      <div class="card-header">
-        <div class="row">
-          <div class="col d-flex justify-content-start" />
-          <div class="col d-flex justify-content-center align-items-center">
-            <h5 class="fw-light mb-0">My skills</h5>
-          </div>
-          <div class="col d-flex justify-content-end">
-            <router-link to="/skills/new_skill" class="btn btn-outline-primary d-inline-flex align-items-center" role="button">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
-                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-              </svg>
-              &nbsp;New
-            </router-link>
-          </div>
+  <Card title="My skills">
+    <template #rightItem>
+      <router-link to="/skills/new_skill" class="btn btn-outline-primary d-inline-flex align-items-center" role="button">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+          <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+        </svg>
+        &nbsp;New
+      </router-link>
+    </template>
+    <div class="list-group list-group-flush">
+      <li
+          v-for="skill in this.mySkills"
+          :key="skill.id"
+          class="list-group-item py-4">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">{{ skill.name }}</h5>
+          <small>{{ skill.url }}</small>
         </div>
-      </div>
-      <div class="card-body">
-        <div class="list-group list-group-flush">
-          <li
-              v-for="skill in mySkills"
-              :key="skill.id"
-              class="list-group-item py-4">
-            <div class="d-flex w-100 justify-content-between">
-              <h5 class="mb-1">{{ skill.name }}</h5>
-              <small>{{ skill.url }}</small>
-            </div>
-            <p class="mb-3">{{ skill.description }}</p>
-            <Status :url="skill.url" />
-            <span v-if="skill.is_published" class="badge bg-info ms-1 p-2">
-              Published
-            </span>
-            <span v-else class="badge bg-secondary ms-1 p-2">
-              Not Published
-            </span>
-            <div class="d-grid gap-2 d-flex mt-2">
-              <router-link :to="{ name: 'skill', params: {id: skill.id}} " class="btn btn-outline-primary" role="button">Edit</router-link>
-              <router-link :to="{ name: 'train', params: {id: skill.id}} " class="btn btn-outline-primary" role="button">Manage Publication</router-link>
-              <button class="btn btn-outline-danger ms-auto" data-bs-toggle="modal" :data-bs-target="`#deleteModal-${skill.id}`">Delete</button>
-              <div class="modal fade" :id="`deleteModal-${skill.id}`" tabindex="-1" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel">Are you sure?</h5>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                      Please confirm that you want to delete <strong>{{ skill.name }}</strong>.
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                      <button type="button" class="btn btn-danger" @click="deleteSkill(skill.id)">Delete</button>
-                    </div>
-                  </div>
+        <p class="mb-3">{{ skill.description }}</p>
+        <Status :url="skill.url" />
+        <span v-if="skill.is_published" class="badge bg-info ms-1 p-2">Published</span>
+        <span v-else class="badge bg-secondary ms-1 p-2">Not Published</span>
+        <div class="d-grid gap-2 d-flex mt-2">
+          <router-link :to="{ name: 'skill', params: {id: skill.id}} " class="btn btn-outline-primary" role="button">Edit</router-link>
+          <router-link :to="{ name: 'train', params: {id: skill.id}} " class="btn btn-outline-primary" role="button">Manage Publication</router-link>
+          <button class="btn btn-outline-danger ms-auto" data-bs-toggle="modal" :data-bs-target="`#deleteModal-${skill.id}`">Delete</button>
+          <div class="modal fade" :id="`deleteModal-${skill.id}`" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Are you sure?</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  Please confirm that you want to delete <strong>{{ skill.name }}</strong>.
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                  <button type="button" class="btn btn-danger" @click="deleteSkill(skill.id)">Delete</button>
                 </div>
               </div>
             </div>
-          </li>
+          </div>
         </div>
-        <div v-if="!mySkills.length" class="d-grid gap-2">
-          <router-link to="/skills/new_skill" class="btn btn-primary d-inline-flex justify-content-center align-items-center" role="button">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
-              <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
-            </svg>
-            &nbsp;New
-          </router-link>
-        </div>
-      </div>
+      </li>
     </div>
-  </div>
+    <div v-if="!this.mySkills.length" class="d-grid gap-2">
+      <router-link to="/skills/new_skill" class="btn btn-primary d-inline-flex justify-content-center align-items-center" role="button">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+          <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
+        </svg>
+        &nbsp;New
+      </router-link>
+    </div>
+  </Card>
 </template>
 
 
 <script>
 import Vue from 'vue'
+import Card from '@/components/Card.vue'
 import Status from '@/components/Status.vue'
 
 export default Vue.component('skills', {
   components: {
+    Card,
     Status
   },
   computed: {
