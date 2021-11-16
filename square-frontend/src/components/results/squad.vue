ea0f5d65c0779ff02fe2adfe0af4d884be5fb19d
Max Eichler <maexe@users.noreply.github.com>
2021-11-16 11:34:57 +0100

    Add dynamic span highlighting

--- square-frontend/src/components/results/squad.vue
+++ square-frontend/src/components/results/squad.vue
@@ -1,26 +1,39 @@
 <!-- Component for the Results. The user can see the results of each chosen skill here. Results can have different formats. -->
 <template>
-  <ul class="list-group list-group-flush">
-    <li
-        v-for="(res, index) in skillResult.results"
-        :key="index"
-        class="list-group-item">
-      <div class="d-flex w-100 justify-content-between align-items-start mb-2">
-        <div>{{ index + 1 }}. Answer: <span class="fw-bold">{{ res.prediction_output.output }}</span></div>
-        <span class="badge bg-primary p-2">{{ roundScore(res.prediction_score) }}%</span>
+  <div>
+    <div class="row mb-4">
+      <div class="col" v-html="currentContext" />
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="list-group list-group-flush">
+          <a
+              v-for="(res, index) in skillResult.results"
+              :key="index"
+              v-on:mouseover="setActive(index, res.prediction_documents[0].span)"
+              class="list-group-item list-group-item-action"
+              :class="{ active: index === activeResult }"
+              :aria-current="index === activeResult"
+              style="cursor: pointer">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">{{ index + 1 }}. Answer</h5>
+              <span class="badge bg-primary p-2">{{ roundScore(res.prediction_score) }}%</span>
+            </div>
+            <small class="mb-1">{{ res.prediction_output.output }}</small>
+            <div class="progress mt-2">
+              <div
+                  class="progress-bar progress-bar-striped bg-primary"
+                  role="progressbar"
+                  :style="{ width: `${roundScore(res.prediction_score)}%` }"
+                  :aria-valuenow="roundScore(res.prediction_score)"
+                  aria-valuemin="0"
+                  aria-valuemax="100" />
+            </div>
+          </a>
+        </div>
       </div>
-      <small v-html="highlightSpan(res.prediction_documents[0].document, res.prediction_documents[0].span)" />
-      <div class="progress mt-2">
-        <div
-            class="progress-bar progress-bar-striped bg-primary"
-            role="progressbar"
-            :style="{ width: `${roundScore(res.prediction_score)}%` }"
-            :aria-valuenow="roundScore(res.prediction_score)"
-            aria-valuemin="0"
-            aria-valuemax="100" />
-      </div>
-    </li>
-  </ul>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -29,6 +42,23 @@ import mixin from '@/components/results/mixin.vue'
 
 export default Vue.component('squad', {
   props: ['skillResult'],
-  mixins: [mixin]
+  mixins: [mixin],
+  data() {
+    return {
+      activeResult: null,
+      span: null
+    }
+  },
+  computed: {
+    currentContext: function () {
+      return this.highlightSpan(this.$store.state.currentContext, this.span)
+    }
+  },
+  methods: {
+    setActive(index, span) {
+      this.activeResult = index
+      this.span = span
+    }
+  }
 })
 </script>
