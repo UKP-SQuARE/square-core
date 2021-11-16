c48b45d9b2ad3ea83a3b38a034c24b4be64963da
Max Eichler <maexe@users.noreply.github.com>
2021-11-16 16:42:30 +0100

    Move options button to card footer

--- square-frontend/src/components/Query.vue
+++ square-frontend/src/components/Query.vue
@@ -88,20 +88,6 @@
                 </div>
               </div>
             </div>
-            <div class="row">
-              <div class="col">
-                <a
-                    class="btn btn-outline-secondary d-inline-flex align-items-center"
-                    :class="{ 'active': showOptions }"
-                    role="button"
-                    v-on:click="showOptions = !showOptions">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sliders" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M11.5 2a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM9.05 3a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0V3h9.05zM4.5 7a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM2.05 8a2.5 2.5 0 0 1 4.9 0H16v1H6.95a2.5 2.5 0 0 1-4.9 0H0V8h2.05zm9.45 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm-2.45 1a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0v-1h9.05z"/>
-                  </svg>
-                  &nbsp;{{ showOptions ? 'Hide' : 'Show' }} expert options
-                </a>
-              </div>
-            </div>
           </div>
           <div v-if="showOptions" class="col-lg mt-3 mt-lg-0">
             <div class="row">
@@ -143,6 +129,18 @@
           </div>
         </div>
       </form>
+    </div>
+    <div class="card-footer bg-white d-flex p-2">
+      <a
+          class="btn btn-outline-secondary d-inline-flex align-items-center ms-auto"
+          :class="{ 'active': showOptions }"
+          role="button"
+          v-on:click="showOptions = !showOptions">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sliders" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M11.5 2a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM9.05 3a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0V3h9.05zM4.5 7a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM2.05 8a2.5 2.5 0 0 1 4.9 0H16v1H6.95a2.5 2.5 0 0 1-4.9 0H0V8h2.05zm9.45 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm-2.45 1a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0v-1h9.05z"/>
+        </svg>
+        &nbsp;{{ showOptions ? 'Hide' : 'Show' }} expert options
+      </a>
     </div>
   </div>
 </template>
