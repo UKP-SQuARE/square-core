841d42df01d4fec95aabb3e5d7a9514e70dd90b4
Max Eichler <maexe@users.noreply.github.com>
2021-11-16 15:45:51 +0100

    Update layout

--- square-frontend/src/components/results/squad.vue
+++ square-frontend/src/components/results/squad.vue
@@ -1,33 +1,28 @@
 <!-- Component for the Results. The user can see the results of each chosen skill here. Results can have different formats. -->
 <template>
   <div>
-    <div class="row mb-4">
-      <div class="col" v-html="currentContext" />
-    </div>
     <div class="row">
-      <div class="col">
+      <div class="col-xl-6 col-md-5 mb-4 mb-md-0">
+        <div class="border rounded bg-light p-4" v-html="currentContext" />
+      </div>
+      <div class="col-xl-6 col-md-7">
         <div class="list-group list-group-flush">
           <a
               v-for="(res, index) in skillResult.results"
               :key="index"
               v-on:mouseover="setActive(index, res.prediction_documents[0].span)"
               class="list-group-item list-group-item-action"
-              :class="{ active: index === activeResult }"
+              :class="{ 'border-primary': index === activeResult }"
               :aria-current="index === activeResult"
               style="cursor: pointer">
-            <div class="d-flex w-100 justify-content-between">
-              <h5 class="mb-1">{{ index + 1 }}. Answer</h5>
-              <span class="badge bg-primary p-2">{{ roundScore(res.prediction_score) }}%</span>
-            </div>
-            <small class="mb-1">{{ res.prediction_output.output }}</small>
-            <div class="progress mt-2">
-              <div
-                  class="progress-bar progress-bar-striped bg-primary"
-                  role="progressbar"
-                  :style="{ width: `${roundScore(res.prediction_score)}%` }"
-                  :aria-valuenow="roundScore(res.prediction_score)"
-                  aria-valuemin="0"
-                  aria-valuemax="100" />
+            <div class="d-flex w-100 justify-content-between align-items-start">
+              <div class="d-flex align-items-baseline">
+                <h5
+                    class="m-0"
+                    :class="{ 'text-primary': index === activeResult }">{{ index + 1 }}.</h5>
+                <small class="mx-2">{{ res.prediction_output.output }}</small>
+              </div>
+              <span class="badge bg-transparent text-dark border border-primary p-2">{{ roundScore(res.prediction_score) }}%</span>
             </div>
           </a>
         </div>
