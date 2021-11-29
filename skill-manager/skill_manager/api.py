edfd5c2b4f3cad0804281203609a901d29b1450e
Tim Baumgärtner <baumgaertner.t@gmail.com>
2021-11-29 19:30:44 +0100

    skill manager inital commit

---
+++ skill-manager/skill_manager/api.py
@@ -0,0 +1,131 @@
+import logging
+from bson import ObjectId
+from itertools import chain
+from typing import List, Optional
+
+import pymongo
+import requests
+from fastapi import FastAPI, Request
+
+from skill_manager.models import Skill, SkillType
+from skill_manager.mongo_settings import MongoSettings
+
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    mongo_settings = MongoSettings()
+    app.state.mongo_client = pymongo.MongoClient(mongo_settings.connection_url)
+    app.state.skill_manager_db = app.state.mongo_client.skill_manager
+
+
+@app.get("/skill-types", response_model=List[str])
+async def get_skill_types():
+    skill_types = [skill_type.value for skill_type in SkillType]
+
+    logger.debug("get_skill_types {skill_types}".format(skill_types))
+    return
+
+
+@app.get("/skill/{id}", response_model=Skill)
+async def get_skill_by_id(id: Optional[str] = None):
+    skill = Skill.from_mongo(
+        app.state.skill_manager_db.skills.find_one({"_id": ObjectId(id)})
+    )
+
+    logger.debug("get_skill_by_id: {skill}".format(skill=skill))
+    return skill
+
+
+@app.get("/skill", response_model=List[Skill])
+async def get_skills(user_id: Optional[str] = None):
+    if user_id:
+        user_skills = app.state.skill_manager_db.skills.find({"user_id": user_id})
+    else:
+        user_skills = []
+    published_skills = app.state.skill_manager_db.skills.find({"published": True})
+    skills = [Skill.from_mongo(s) for s in chain(user_skills, published_skills)]
+
+    logger.debug("get_skills: {skills}".format(skills=skills))
+    return skills
+
+
+@app.post("/skill", response_model=Skill, status_code=201)
+async def create_skill(skill: Skill):
+
+    skill_id = app.state.skill_manager_db.skills.insert_one(skill.mongo()).inserted_id
+    skill = await get_skill_by_id(skill_id)
+
+    logger.debug("create_skill: {skill}".format(skill=skill))
+    return skill
+
+
+@app.put("/skill/{id}", response_model=Skill)
+async def update_skill(id: str, request: Request):
+    request = await request.json()
+    skill = await get_skill_by_id(id)
+
+    for k, v in request.items():
+        if hasattr(skill, k):
+            setattr(skill, k, v)
+    
+    _ = app.state.skill_manager_db.skills.find_one_and_update(
+        {"_id": ObjectId(id)}, {"$set": skill.mongo()}
+    )
+    updated_skill = await get_skill_by_id(id)
+
+    logger.debug(
+        "update_skill: old: {skill} updated: {updated_skill}".format(
+            skill=skill, updated_skill=updated_skill
+        )
+    )
+    return skill
+
+
+@app.delete("/skill/{id}", status_code=204)
+async def delete_skill(id: str):
+    delete_result = app.state.skill_manager_db.skills.delete_one({"_id": ObjectId(id)})
+    logger.debug("delete_skill: {id}".format(id=id))
+    if delete_result.acknowledged:
+        return
+    else:
+        raise RuntimeError(delete_result.raw_result)
+
+
+@app.post("/skill/{id}/publish", response_model=Skill, status_code=201)
+async def publish_skill(id: str):
+    skill = await get_skill_by_id(id)
+    skill.published = True
+    skill = await update_skill(id, skill)
+
+    logger.debug("publish_skill: {skill}".format(skill=skill))
+    return skill
+
+
+@app.post("/skill/{id}/unpublish", response_model=Skill, status_code=201)
+async def unpublish_skill(id: str):
+    skill = await get_skill_by_id(id)
+    skill.published = False
+    skill = await update_skill(id, skill)
+
+    logger.debug("unpublish_skill: {skill}".format(skill=skill))
+    return skill
+
+
+@app.post("/skill/{id}/query")
+async def query_skill(request: Request, id: str):
+    request = await request.json()
+    skill = await get_skill_by_id(id)
+    response = requests.post(f"{skill.url}/query", json=request)
+    response.raise_for_status()
+
+    prediction = response.json()
+    logger.debug(
+        "query_skill: request: {request} prediction: {prediction}".format(
+            request=request, prediction=prediction
+        )
+    )
+    return prediction
---
+++ skill-manager/skill_manager/models.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel, Field, validator
+
+from skill_manager.mongo_model import MongoModel
+from skill_manager.py_object_id import PyObjectId
+
+
+class SkillType(str, Enum):
+    abstractive = "abstractive"
+    span_extraction = "span extraction"
+    multiple_choiche = "mulitple-choice"
+    categorical = "categorical"
+    open_domain = "open-domain"
+
+class SkillSettings(BaseModel):
+    requires_context: bool = False
+    requires_multiple_choices: int = Field(0, ge=0)
+
+class Skill(MongoModel):
+    id: Optional[PyObjectId]
+    name: str
+    url: str
+    skill_type: SkillType
+    skill_settings: SkillSettings
+    user_id: str
+    created_at: datetime = Field(default_factory=datetime.now)
+    description: str = None
+    published: bool = False
+
+    @validator("url")
+    def validate_url(cls, url):
+        if not url.startswith("http"): raise ValueError(url)
+        if url.endswith("/"): url = url[:-1]
+        return url
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "HAL9000",
+                "url": "http://h.al:9000",
+                "description": "Heuristically programmed Algorithmic computer",
+                "skill_type": "abstractive",
+                "skill_settings": {
+                    "requires_context": False,
+                    "requires_multiple_choices": 0
+                },
+                "user_id": "Dave",
+                "created_at": "1992-01-12T09:00:00.000000",
+                "published": False
+            }
+        }
+
