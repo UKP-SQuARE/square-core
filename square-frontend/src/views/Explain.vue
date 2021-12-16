bec4a088be07bd5ee8d59a5fa96d38ea755d5796
Max Eichler <maexe@users.noreply.github.com>
2021-12-16 10:44:36 +0100

    Add skill selector

--- square-frontend/src/views/Explain.vue
+++ square-frontend/src/views/Explain.vue
@@ -1,10 +1,35 @@
 <!-- The Explainability Page. Contains information about this project -->
 <template>
   <div>
-    <div class="row justify-content-center">
-      <div class="col-sm-10 col-md-8 col-lg-6">
-        <h1 class="fw-light">Explainability</h1>
-        <p class="lead">🚧</p>
+    <div class="card border-primary shadow">
+      <div class="card-body p-4">
+        <form v-on:submit.prevent="askQuestion">
+          <div class="row">
+            <div class="col-lg">
+              <div class="row">
+                <div class="col">
+                  <div class="input-group">
+                    <div class="form-floating flex-grow-1">
+                      <select v-model="options.selectedSkill" class="form-select rounded-0 rounded-start" id="skillSelect">
+                        <option v-for="skill in availableSkills" v-bind:value="skill.id" v-bind:key="skill.id">
+                          {{ skill.name }} — {{ skill.description }}
+                        </option>
+                      </select>
+                      <label for="skillSelect" class="form-label col-form-label-sm text-muted">Skill selector</label>
+                    </div>
+                    <button
+                        class="btn btn-lg btn-primary d-inline-flex align-items-center"
+                        type="submit"
+                        :disabled="waiting">
+                      <span v-show="waiting" class="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
+                      &nbsp;Run Checklist
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -13,5 +38,34 @@
 <script>
 import Vue from 'vue'
 
-export default Vue.component('explainability-page', {})
+export default Vue.component('explainability-page', {
+  data() {
+    return {
+      waiting: false,
+      options: {
+        selectedSkill: ''
+      }
+    }
+  },
+  computed: {
+    availableSkills() {
+      return this.$store.state.availableSkills
+    }
+  },
+  /**
+   * Make the store update the skills and init the explain options
+   */
+  beforeMount() {
+    this.$store.dispatch('updateSkills')
+        .then(() => {
+          this.$store.commit('initExplainOptions', {})
+        })
+        .then(() => {
+          // Copy the object so we do not change the state before a query is issued
+          this.options = JSON.parse(
+              JSON.stringify(this.$store.state.explainOptions)
+          )
+        })
+  }
+})
 </script>
