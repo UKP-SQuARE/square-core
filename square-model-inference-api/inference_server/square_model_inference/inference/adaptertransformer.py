c0c88c68b898bd63e937414568476f319e181c6f
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-01-20 13:16:39 +0100

    dynamic number of answers in sequence clf

--- square-model-inference-api/inference_server/square_model_inference/inference/adaptertransformer.py
+++ square-model-inference-api/inference_server/square_model_inference/inference/adaptertransformer.py
@@ -90,11 +90,12 @@ class AdapterTransformer(Transformer):
         # We only have to change the label2id mapping from config.label2id (what super() uses) to the mapping
         # of the chosen head
         logger.info(f"sequence classification request:\n{request.json()}")
+        
+        self.model.config.prediction_heads[request.adapter_name]["num_choices"] = len(request.input)
         prediction = super()._sequence_classification(request)
 
-        label2id = self.model.config.prediction_heads[request.adapter_name]["label2id"]
-        id2label = {v:k for k,v in label2id.items()}
-        prediction.id2label = id2label
+        # id2label = {v:k for k,v in label2id.items()}
+        # prediction.id2label = id2label
 
         logger.info(f"sequence classification prediction:\n{prediction}")
         return prediction
