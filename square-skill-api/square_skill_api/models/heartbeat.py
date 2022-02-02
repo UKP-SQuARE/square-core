5447a15567d9a13772094807e1f0fc4e1b8d019d
Rachneet <rachneetsachdeva@yahoo.com>
2022-02-02 11:52:33 +0100

    chore:remove unwanted files

--- square-skill-api/square_skill_api/models/heartbeat.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class HeartbeatResult(BaseModel):
-    is_alive: bool = Field(
-        ..., description="`True` if Skill is up and running, else `False`"
-    )
