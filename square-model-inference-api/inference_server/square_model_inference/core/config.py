3b52c8653d0eebd32b094850f9beb3c80c210bf8
Rachneet <rachneetsachdeva@yahoo.com>
2022-02-22 18:14:16 +0100

    feat: add openapi url

--- square-model-inference-api/inference_server/square_model_inference/core/config.py
+++ square-model-inference-api/inference_server/square_model_inference/core/config.py
@@ -7,6 +7,8 @@ from starlette.config import Config
 APP_VERSION = "0.1.0"
 APP_NAME = "SQuARE Model Inference API"
 API_PREFIX = "/api"
+OPENAPI_URL= "/api/openapi.json"
+
 
 @dataclass
 class ModelConfig(Mapping):
