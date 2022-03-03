d34a9727c17fabd37e5701c4efe785948ce2ac87
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-03-03 10:49:34 +0100

    expose port 80 by default

--- skills/make_dockerfile.sh
+++ skills/make_dockerfile.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 SKILL=$1
-PORT=$2
 
-sed -e "s/%%SKILL%%/$SKILL/g" -e "s/%%PORT%%/$PORT/g" Dockerfile.template > "$SKILL.Dockerfile"
+sed -e "s/%%SKILL%%/$SKILL/g" Dockerfile.template > "$SKILL.Dockerfile"
