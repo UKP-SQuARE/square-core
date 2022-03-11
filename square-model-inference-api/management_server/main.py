d39636175970658633a7fc1f4a244aaab36860b2
Rachneet <rachneetsachdeva@yahoo.com>
2022-03-11 11:13:37 +0100

    feat: update openapi paths

--- square-model-inference-api/management_server/main.py
+++ square-model-inference-api/management_server/main.py
@@ -1,8 +1,10 @@
+import os
 import logging
 from logging.config import fileConfig
 
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.utils import get_openapi
 
 from app.core.config import settings
 from app.routers.api import api_router
@@ -38,7 +40,34 @@ def get_app() -> FastAPI:
     return fast_app
 
 
+def custom_openapi():
+    """
+    change api paths as per the end-user requirements
+    """
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title=settings.APP_NAME,
+        version=settings.APP_VERSION,
+        description="API reference for model management.",
+        routes=app.routes,
+    )
+    replaced_keys = dict()
+    prefix = os.getenv("API_PREFIX", "models")
+    for api in openapi_schema["paths"].keys():
+        api_split = list(api.split("/"))
+        api_split.insert(2, prefix)
+        api_mod = "/".join(api_split)
+        replaced_keys[api] = api_mod
+
+    new_openapi_paths = {replaced_keys[k]: v for k, v in openapi_schema["paths"].items()}
+    openapi_schema["paths"] = new_openapi_paths
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
 app = get_app()
+app.openapi_schema = custom_openapi()
 
 if __name__ == "__main__":
     import uvicorn
