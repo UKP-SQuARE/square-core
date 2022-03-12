a5fa63ff16da7fe2d45d23f0cc62b331b5dfdd02
Hannah <hsterz16@gmail.com>
2022-03-12 18:52:51 +0100

    Change backend to redis

--- square-model-inference-api/management_server/tasks/celery.py
+++ square-model-inference-api/management_server/tasks/celery.py
@@ -1,13 +1,19 @@
 from celery import Celery
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
+user = os.getenv("USERNAME", "user")
+password = os.getenv("PASSWORD", "user")
 
 app = Celery('tasks',
-             backend='rpc://',
-             broker="amqp://guest:guest@rabbit:5672//",
+             backend="redis://redishost",
+             broker=f"amqp://{user}:{password}@rabbit:5672//",
              include=['tasks.tasks'])
+
+logger.info(password)
+logger.info(f"amqp://{user}:{password}@rabbit:5672//")
 
 if __name__ == '__main__':
     app.start()
