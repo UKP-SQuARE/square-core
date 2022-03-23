9eb26df2872cbe8827b9249aa1a65486c56949c3
Rachneet <rachneetsachdeva@yahoo.com>
2022-03-23 14:19:34 +0100

    feat: create mongo class

--- square-model-inference-api/management_server/mongo_access.py
+++ square-model-inference-api/management_server/mongo_access.py
@@ -1,63 +1,76 @@
 from pymongo import MongoClient
+from app.core.mongo_config import MongoSettings
+
 import logging
-
-# ToDo make this environment variables
-username = "root"
-password = "example"
-
-client = MongoClient('mongodb://%s:%s@%s' % (username, password, "mongo"))
 logger = logging.getLogger(__name__)
 
 
-async def add_model_db(identifier, env):
-    data = env.copy()
-    data["identifier"] = identifier
-    db = client.model_management
-    models = db.models
+class MongoClass:
+    def __init__(self):
+        """
+        get mongo settings and initialize db
+        """
+        mongo_settings = MongoSettings()
+        self.client = MongoClient(mongo_settings.connection_url)
+        self.db = self.client.model_management  # database
+        self.models = self.db.models            # collection
 
-    if models.count_documents({"identifier": identifier}) >= 1:
-        return False
+    def close(self):
+        """
+        close mongo db connection
+        """
+        self.client.close()
 
-    models.insert_one(data)
-    return True
+    async def add_model_db(self, identifier, env):
+        """
+        add entry to the db
+        """
+        data = env.copy()
+        data["identifier"] = identifier
 
+        if self.models.count_documents({"identifier": identifier}) >= 1:
+            return False
 
-async def remove_model_db(identifier):
-    query = {"identifier": identifier}
-    db = client.model_management
-    models = db.models
-    models.delete_one(query)
+        self.models.insert_one(data)
+        return True
 
+    async def remove_model_db(self, identifier):
+        """
+        remove entry from db
+        """
+        query = {"identifier": identifier}
+        self.models.delete_one(query)
 
-async def get_models_db():
-    db = client.model_management
-    models = db.models
-    results = []
-    for m in models.find():
-        logger.info("Result type: {}".format(type(m)))
-        results.append(m)
-    return results
+    async def get_models_db(self):
+        """
+        get db entries
+        """
+        results = []
+        for m in self.models.find():
+            logger.info("Result type: {}".format(type(m)))
+            results.append(m)
+        return results
 
+    async def update_model_db(self, identifier, updated_params):
+        """
+        Update db entries
+        """
+        query = {"identifier": identifier}
+        new_values = {"$set": {
+            "MAX_INPUT_SIZE": updated_params.max_input,
+            "DISABLE_GPU": updated_params.disable_gpu,
+            "BATCH_SIZE": updated_params.batch_size,
+            "RETURN_PLAINTEXT_ARRAYS": updated_params.return_plaintext_arrays,
+        }}
+        self.models.update_one(query, new_values)
 
-async def update_model_db(identifier, updated_params):
-    db = client.model_management
-    models = db.models
-    query = {"identifier": identifier}
-    new_values = {"$set": {
-        "MAX_INPUT_SIZE": updated_params.max_input,
-        "DISABLE_GPU": updated_params.disable_gpu,
-        "BATCH_SIZE": updated_params.batch_size,
-        "RETURN_PLAINTEXT_ARRAYS": updated_params.return_plaintext_arrays,
-    }}
-    models.update_one(query, new_values)
-
-
-async def init_db(deployed_models):
-    db = client.model_management
-    models = db.models
-    added_models = []
-    for data in deployed_models:
-        if models.count_documents({"identifier": data["identifier"]}) == 0:
-            models.insert_one(data)
-            added_models.append(data["identifier"])
-    return added_models
+    async def init_db(self, deployed_models):
+        """
+        add deployed models to db
+        """
+        added_models = []
+        for data in deployed_models:
+            if self.models.count_documents({"identifier": data["identifier"]}) == 0:
+                self.models.insert_one(data)
+                added_models.append(data["identifier"])
+        return added_models
