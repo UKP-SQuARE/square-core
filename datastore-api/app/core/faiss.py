12c9bde7f4f8b2fafc5cd6890abf923dc6420804
kwang2049 <kexin.wang.2049@gmail.com>
2022-03-28 13:59:57 +0200

    make faiss containers visible only within the docker network

--- datastore-api/app/core/faiss.py
+++ datastore-api/app/core/faiss.py
@@ -15,8 +15,12 @@ class FaissClient:
     def __init__(self, base_url: str):
         self.base_url = base_url
 
+    def build_faiss_url(self, datastore_name, index_name):
+        url = '_'.join(['faiss', datastore_name, index_name])
+        return url
+
     def status(self, datastore_name, index_name) -> Optional[dict]:
-        url = f"{self.base_url}/{datastore_name}/{index_name}/index_list"
+        url = f"{self.build_faiss_url(datastore_name, index_name)}/index_list"
         response = requests.get(url)
         if response.status_code != 200:
             logger.info(response.text)
@@ -26,7 +30,7 @@ class FaissClient:
         return queried
 
     def search(self, datastore_name, index_name, query_vector, top_k=10) -> List[QueryResult]:
-        url = f"{self.base_url}/{datastore_name}/{index_name}/search"
+        url = f"{self.build_faiss_url(datastore_name, index_name)}/search"
         data = {"k": top_k, "vectors": [query_vector]}
         logger.debug(f"querying faiss at {url}") 
         response = requests.post(url, json=data)
@@ -40,7 +44,7 @@ class FaissClient:
         return queried
 
     def explain(self, datastore_name, index_name, query_vector, document_id) -> QueryResult:
-        url = f"{self.base_url}/{datastore_name}/{index_name}/explain"
+        url = f"{self.build_faiss_url(datastore_name, index_name)}/explain"
         data = {"vector": query_vector, "id": document_id}
         response = requests.post(url, json=data)
         if response.status_code != 200:
@@ -51,7 +55,7 @@ class FaissClient:
         return queried
 
     def reconstruct(self, datastore_name, index_name, document_id) -> List[float]:
-        url = f"{self.base_url}/{datastore_name}/{index_name}/reconstruct"
+        url = f"{self.build_faiss_url(datastore_name, index_name)}/reconstruct"
         params = {"id": document_id}
         response = requests.get(url, params=params)
         if response.status_code != 200:
