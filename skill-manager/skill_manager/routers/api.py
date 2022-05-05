b8a4be0ded4c980e7421ea7e34387bf2ee06bab1
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-05-05 17:45:21 +0200

    add skill deployments router

--- skill-manager/skill_manager/routers/api.py
+++ skill-manager/skill_manager/routers/api.py
@@ -4,6 +4,7 @@ from skill_manager.routers.health import router as health_router
 from skill_manager.routers.skill import router as skill_router
 from skill_manager.routers.skill_types import router as skill_types_router
 from skill_manager.routers.skill_templates import router as skill_templates_router
+from skill_manager.routers.skill_deployments import router as skill_deployments_router
 
 router = APIRouter()
 
@@ -11,3 +12,4 @@ router.include_router(health_router)
 router.include_router(skill_router)
 router.include_router(skill_types_router)
 router.include_router(skill_templates_router)
+router.include_router(skill_deployments_router)
