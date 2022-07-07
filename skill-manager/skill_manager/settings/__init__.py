121baab85b1fe775ac8b265d3410b576fcc413d4
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-07-07 17:02:10 +0200

    add model management client

--- skill-manager/skill_manager/settings/__init__.py
+++ skill-manager/skill_manager/settings/__init__.py
@@ -1,2 +1,3 @@
 from skill_manager.settings.keycloak_settings import KeycloakSettings
 from skill_manager.settings.mongo_settings import MongoSettings
+from skill_manager.settings.model_management_settings import ModelManagementSettings
