0a86b4f13918f4b6d03ce938a9778edef2774e32
Zhang,Hao <hao.zhang@stud.tu-darmstadt.de>
2022-07-09 09:57:24 +0000

    update

--- square-frontend/src/components/modals/ExplainOutput.vue
+++ square-frontend/src/components/modals/ExplainOutput.vue
@@ -6,17 +6,31 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" />
         </div>
         <div class="modal-body">
-          <div class="text-center">
+          <!-- <div class="text-center">
             <h3 class="card-title mb-3">Explain the output</h3>
-          </div>
+          </div> -->
         </div>
 
    
+        <!-- <div>
+           <h3>{{test}}</h3> 
+        </div> -->
+        <div class="row"> 
+          <h4 class="col-sm-2">Method:
+         
+          </h4>
+            
+            <button v-on:click="greet" type="button" class="btn btn-default col-sm-2">Attention</button>
+            <button type="button" class="btn btn-default col-sm-2">Scaled Attention</button>
+            <button type="button" class="btn btn-default col-sm-2">Simple Grad</button>
+            <button type="button" class="btn btn-default col-sm-2">Smooth Grad</button>
+            <button type="button" class="btn btn-default col-sm-2">Integrated Grad</button>
 
-        <div> 
-          <h4>Method:</h4>
+
 
         </div>
+          
+
         <div> 
           <h4>Question:</h4>
 
@@ -36,18 +50,27 @@
 import Vue from 'vue'
 //import BadgePopover from '../BadgePopover'
 import mixin from '@/components/results/mixin.vue'
+import { postQuery } from '../../api'
 
 
 
 export default Vue.component("explain-output",{
+  // data () { return {
+  //   count: 0
+  // }},
   props:['test'],  //args should be the test json file
   mixins:[mixin], // ??
   components:{
     //BadgePopover // maybe useful
   },
-  method:{
+  methods:{
     // write functions here
+    postReq() {
 
+      postQuery()
+    },
+    greet: function() {
+      alert(this.$store)    }
   },
 }
 )
