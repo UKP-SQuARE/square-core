815754c0837afb5657a3ed147714897c853b0480
Haritz Puerto <haritzpuerto94@gmail.com>
2022-07-18 11:53:21 +0200

    added news page

---
+++ square-frontend/src/views/News.vue
@@ -0,0 +1,27 @@
+<!-- The feedback page. -->
+<template>
+  <div class="d-flex align-items-top">
+      <div class="d-flex flex-column flex-md-row w-100 ps-md-3">
+        <div class="bg-light flex-fill my-2 me-md-3 pt-3 px-3 pt-md-5 px-md-5 overflow-hidden">
+          <div class="mb-3 pb-3">
+            <h2 class="display-5">News</h2>
+            <ul>
+              <li>July 2022: SQuARE is presented at the <a href="https://scads.ai/education/summer-schools/scads-ai-summer-school-2022/prof-iryna-gurevychharitz-puerto/" target="_blank">ScaDS.AI Machine Learning Summer School</a> in Leipzig. Check the <a href="https://twitter.com/Sca_DS/status/1546789948644286465" target="_blank">Tweet</a>.</li>
+              <li>May 2022: SQuARE is presented at ACL as a <a href="https://aclanthology.org/2022.acl-demo.2/" target="_blank">demo paper</a>.</li>
+              <li>March 2022: SQuARE is publicly launched!</li>
+              <li>Feb 2022: Haritz Puerto talks about QA, <a href="https://arxiv.org/abs/2112.01922" target="_blank">MetaQA</a>, and introduces the soon-to-be-launched SQuARE at the <a href="https://anchor.fm/elia-ia/episodes/Ep-14---Haritz-Puerto-UKP-Lab---Haciendo-que-la-IA-resuelva-preguntas-e1ekdgl" target="_blank"> We Decentralize Tech Podcast</a> (in Spanish). </li>
+            </ul>
+
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+
+<script>
+import Vue from 'vue'
+
+export default Vue.component('show-news', {})
+</script>
+
