dce9db98a498a81a64dd03ca48f6850497300893
Hossain Shaikh Saadi <shaikh.saadi@tum.de>
2022-07-24 11:06:17 +0200

    updated router script

--- explainability-api/app/api/router.py
+++ explainability-api/app/api/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api import heartbeat, checklist_api
+from app.api import heartbeat, attack_api , checklist_api
 
 api_router = APIRouter()
 api_router.include_router(heartbeat.router, tags=["health"], prefix="/health")
 api_router.include_router(checklist_api.router, tags=["checklist"], prefix="/checklist")
+api_router.include_router(attack_api.router, tags=["attack"], prefix="/attack")
