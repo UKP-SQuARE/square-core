9e773ac31101255770fc37deaa9235f32eab3164
Haritz Puerto <haritzpuerto94@gmail.com>
2022-07-24 14:15:44 +0200

    using a real graph

--- square-frontend/src/views/GraphViz.vue
+++ square-frontend/src/views/GraphViz.vue
@@ -3,7 +3,7 @@
     id="container"
     class="d-flex flex-column justify-content-center align-items-center"
   >
-    <h1>Data flow Graph</h1>
+    <h1>Graph Viz</h1>
     <h4 v-if="loading">Loading</h4>
     <h4 v-if="error" class="text-danger">{{ error }}</h4>
     <div id="cy" class="cy"></div>
@@ -14,99 +14,101 @@
 <script>
 import cydagre from "cytoscape-dagre";
 import cytoscape from "cytoscape";
+import graph from './graph_example.json'
 
-const nodes = [
-  {
-    data: {
-      id: 0,
-      name: "Q_Node",
-      description: "",
-      q_node: true,
-      ans_node: false,
-      width: 140,
-    },
-  },
-  {
-    data: {
-      id: 1,
-      name: "Mammal",
-      description: "",
-      q_node: false,
-      ans_node: false,
-      width: 140,
-    },
-  },
-  {
-    data: {
-      id: 2,
-      name: "Reptile",
-      description: "",
-      q_node: false,
-      ans_node: false,
-      width: 140,
-    },
-  },
-  {
-    data: {
-      id: 3,
-      name: "Horse",
-      description: "",
-      q_node: false,
-      ans_node: false,
-      width: 140,
-    },
-  },
-  {
-    data: {
-      id: 4,
-      name: "Dog",
-      description: "Join",
-      q_node: false,
-      ans_node: false,
-      width: 140,
-    },
-  },
-  {
-    data: {
-      id: 5,
-      name: "Goat",
-      description: "Branch Out",
-      q_node: false,
-      ans_node: false,
-      width: 140,
-    },
-  },
-  {
-    data: {
-      id: 6,
-      name: "Hound",
-      description: "",
-      q_node: false,
-      ans_node: false,
-      width: 140,
-    },
-  },
-  {
-    data: {
-      id: 7,
-      name: "German Shephard",
-      description: "",
-      q_node: false,
-      ans_node: true,
-      width: 140,
-    },
-  },
-];
 
-const edges = [
-  { data: { source: 0, target: 1, label: "Sub", width: 10 } },
-  { data: { source: 0, target: 2, label: "Sub", width: 20  } },
-  { data: { source: 1, target: 3, label: "Sub", width: 30  } },
-  { data: { source: 1, target: 4, label: "Sub", width: 40  } },
-  { data: { source: 1, target: 5, label: "Sub", width: 50  } },
-  { data: { source: 4, target: 6, label: "Sub", width: 60  } },
-  { data: { source: 4, target: 7, label: "Sub", width: 70  } },
-];
+// const nodes = [
+//   {
+//     data: {
+//       id: 0,
+//       name: "Q_Node",
+//       description: "",
+//       q_node: true,
+//       ans_node: false,
+//       width: 140,
+//     },
+//   },
+//   {
+//     data: {
+//       id: 1,
+//       name: "Mammal",
+//       description: "",
+//       q_node: false,
+//       ans_node: false,
+//       width: 140,
+//     },
+//   },
+//   {
+//     data: {
+//       id: 2,
+//       name: "Reptile",
+//       description: "",
+//       q_node: false,
+//       ans_node: false,
+//       width: 140,
+//     },
+//   },
+//   {
+//     data: {
+//       id: 3,
+//       name: "Horse",
+//       description: "",
+//       q_node: false,
+//       ans_node: false,
+//       width: 140,
+//     },
+//   },
+//   {
+//     data: {
+//       id: 4,
+//       name: "Dog",
+//       description: "Join",
+//       q_node: false,
+//       ans_node: false,
+//       width: 140,
+//     },
+//   },
+//   {
+//     data: {
+//       id: 5,
+//       name: "Goat",
+//       description: "Branch Out",
+//       q_node: false,
+//       ans_node: false,
+//       width: 140,
+//     },
+//   },
+//   {
+//     data: {
+//       id: 6,
+//       name: "Hound",
+//       description: "",
+//       q_node: false,
+//       ans_node: false,
+//       width: 140,
+//     },
+//   },
+//   {
+//     data: {
+//       id: 7,
+//       name: "German Shephard",
+//       description: "",
+//       q_node: false,
+//       ans_node: true,
+//       width: 140,
+//     },
+//   },
+// ];
+
+// const edges = [
+//   { data: { source: 0, target: 1, label: "Sub", width: 10 } },
+//   { data: { source: 0, target: 2, label: "Sub", width: 20  } },
+//   { data: { source: 1, target: 3, label: "Sub", width: 30  } },
+//   { data: { source: 1, target: 4, label: "Sub", width: 40  } },
+//   { data: { source: 1, target: 5, label: "Sub", width: 50  } },
+//   { data: { source: 4, target: 6, label: "Sub", width: 60  } },
+//   { data: { source: 4, target: 7, label: "Sub", width: 70  } },
+// ];
 
 export default {
   name: "DataFlow",
@@ -178,19 +180,37 @@ export default {
           fit: true,
         },
       });
-      for (const n of nodes){
-          cy.add({
-            data: n['data']
-          });
-          // add edges from Z to all nodes
+      /* eslint-disable */
+      var cnt = 0;
+      for (const [key, node] of Object.entries(graph["nodes"]["statement_0"])) {
+        cy.add({
+          data: node
+        });
+        cnt += 1;
+        if (cnt == 10) {
+          break;
+        }
       }
-      for (const e of edges){
-          e['data']['opacity'] = e['data']['width']/100;
-          cy.add({
-            data: e['data']
-          });
-          // add edges from Z to all nodes
+      /* eslint-disable */
+      for (const [key, edge] of Object.entries(graph["edges"]["statement_0"])) {
+        cy.add({
+          data: edge
+        });
       }
+      // for (const n of nodes){
+      //     cy.add({
+      //       data: n['data']
+      //     });
+      //     // add edges from Z to all nodes
+      // }
+      // var edges = graph["edges"]["statement_0"]
+      // for (const e of edges){
+      //     e['data']['opacity'] = e['data']['width']/100;
+      //     cy.add({
+      //       data: e['data']
+      //     });
+      //     // add edges from Z to all nodes
+      // }
       cy.layout({ 
           name: 'circle'
         }).run();
