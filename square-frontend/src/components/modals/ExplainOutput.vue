3c6f86ba03abf2110bc787b6c53311d207fba8a2
Haritz Puerto <haritzpuerto94@gmail.com>
2022-08-11 10:43:34 +0200

    show error msg when explain fails

--- square-frontend/src/components/modals/ExplainOutput.vue
+++ square-frontend/src/components/modals/ExplainOutput.vue
@@ -8,7 +8,14 @@
         <div class="modal-body">
           <div class="container text-center">
             <div class="alert alert-warning" v-if="this.$store.state.loadingExplainability" role="alert">
-                  It's taking a bit longer than usual to generate your output... Please hang in there.
+              It's taking a bit longer than usual to generate your output... Please hang in there.
+            </div>
+            <div class="alert alert-warning" v-if="failure" :dismissible="true" role="alert">
+              An error occurred 
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-frown" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.498 3.498 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.498 4.498 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
+              </svg>
             </div>
 
             <div class="row">
@@ -133,6 +140,7 @@ export default Vue.component("explain-output",{
       waiting_smooth_grads: false,
       waiting_integrated_grads: false,
       show_saliency_map: false,
+      failure: false,
       }
   },
   components:{
@@ -146,7 +154,7 @@ export default Vue.component("explain-output",{
   },
   methods:{
     postReq(method) {
-      console.log(this.$store.state.loadingExplainability)
+      this.failure = false;
       // num words in context
       var numQuestionWords = tokenize({'text': this.$store.state.currentQuestion, 'includePunctuation': true}).length;
       // num_Maxshow for explain method is the min(numWords, numContextWords)
@@ -154,7 +162,6 @@ export default Vue.component("explain-output",{
       if (!this.$store.state.currentResults[0].skill.skill_settings.requiresContext) { // for ODQA
         let context = this.$store.state.currentResults[0].predictions[0].prediction_documents[0].document
         numContextWords = tokenize({'text': context, 'includePunctuation': true}).length;
-
       }
       this.num_Maxshow = Math.max(numQuestionWords, numContextWords);
       // method for setting explain method : 'attention', 'scaled_attention', 'simple_grads', 'smooth_grads', 'integrated_grads'      
@@ -201,11 +208,21 @@ export default Vue.component("explain-output",{
       }).then(() => {
         this.failure = false,
         this.num_show = 3
+        this.show_saliency_map = true;
+        // add class active to the button method+"_btn"
+        document.getElementById(method+"_btn").classList.add("active");
+        // the tokenizer used by the API is a bit different from the one used in the fronted,
+        // so update num_Maxshow with the real number of words in the context
+        this.num_Maxshow = this.$store.state.currentResults[0].predictions[0].attributions.topk_context_idx.length;
       }).catch(() => {
         this.failure = true
       }).finally(() => {
         // switch the waiting_* variables to false to stop loading spinner
-        switch(method){
+        this.stopSpinner(method);
+      })
+    },
+    stopSpinner(method){
+      switch(method){
           case 'attention':
             this.waiting_attention = false;
             break;
@@ -222,15 +239,7 @@ export default Vue.component("explain-output",{
             this.waiting_integrated_grads = false;
             break;
         }
-        this.show_saliency_map = true;
-        // add class active to the button method+"_btn"
-        document.getElementById(method+"_btn").classList.add("active");
-        // the tokenizer used by the API is a bit different from the one used in the fronted,
-        // so update num_Maxshow with the real number of words in the context
-        this.num_Maxshow = this.$store.state.currentResults[0].predictions[0].attributions.topk_context_idx.length;
-      })
     },
-
     highLight(topk_idx,attributions){ // add here skill param
       var listWords = [];
       for (var i = 0; i < attributions.length; i++) {
