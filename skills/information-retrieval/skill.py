25204a324eb2debdb791f8d8224467ee2ff12be0
Serwar <serwarde@gmail.com>
2022-09-23 15:36:04 +0200

    Add new information retrieval skill

---
+++ skills/information-retrieval/skill.py
@@ -0,0 +1,36 @@
+import logging
+from typing import Iterable
+
+from square_skill_api.models import QueryOutput, QueryRequest
+
+from square_skill_helpers import DataAPI, ModelAPI
+
+logger = logging.getLogger(__name__)
+
+model_api = ModelAPI()
+data_api = DataAPI()
+
+
+async def predict(request: QueryRequest) -> QueryOutput:
+    """Given a question, performs open-domain, extractive QA. First, background
+    knowledge is retrieved using a specified index and retrieval method. Next, the top k
+    documents are used for span extraction. Finally, the extracted answers are returned.
+    """
+
+    query = request.query
+
+    data = await data_api(
+        datastore_name=request.skill_args["datastore"],
+        index_name=request.skill_args.get("index", ""),
+        query=query,
+    )
+    
+    logger.info(f"Data API output:\n{data}")
+    context = [d["document"]["text"] for d in data]
+    context_score = [d["score"] for d in data]
+
+    return QueryOutput.from_information_retrieval(
+        questions=query,
+        context=context,
+        context_score=context_score,
+    )
