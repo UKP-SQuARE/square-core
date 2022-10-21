201f86c842ee980b190704a8b02120ba621a45e1
Haritz Puerto <haritzpuerto94@gmail.com>
2022-10-21 15:08:35 +0200

    bold conferences

--- square-frontend/src/views/Publications.vue
+++ square-frontend/src/views/Publications.vue
@@ -8,7 +8,7 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="aacl2022_square_header">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#aacl2022_square" aria-expanded="false" aria-controls="aacl2022_square">
-                                [AACL, 2022] UKP-SQuARE v2: Explainability and Adversarial Attacks for Trustworthy QA
+                                [<b>AACL</b>, 2022] UKP-SQuARE v2: Explainability and Adversarial Attacks for Trustworthy QA
                             </button>
                             </h2>
                             <div id="aacl2022_square" class="accordion-collapse collapse" aria-labelledby="aacl2022_square_header" data-bs-parent="#accordion_square_papers">
@@ -22,7 +22,7 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="acl2022_square_header">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#acl2022_square" aria-expanded="false" aria-controls="acl2022_square">
-                                [ACL, 2022] UKP-SQUARE: An Online Platform for Question Answering Research
+                                [<b>ACL</b>, 2022] UKP-SQUARE: An Online Platform for Question Answering Research
                             </button>
                             </h2>
                             <div id="acl2022_square" class="accordion-collapse collapse" aria-labelledby="acl2022_square_header" data-bs-parent="#accordion_square_papers">
@@ -57,7 +57,7 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="naacl2022_kexin_header">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#naacl2022_kexin" aria-expanded="false" aria-controls="naacl2022_kexin">
-                                [NAACL, 2022] GPL: Generative Pseudo Labeling for Unsupervised Domain Adaptation of Dense Retrieval.
+                                [<b>NAACL</b>, 2022] GPL: Generative Pseudo Labeling for Unsupervised Domain Adaptation of Dense Retrieval.
                             </button>
                             </h2>
                             <div id="naacl2022_kexin" class="accordion-collapse collapse" aria-labelledby="naacl2022_kexin_header" data-bs-parent="#accordion_funded_by_square">
@@ -71,7 +71,7 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="neurips2021_nandan_header">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#neurips2021_nandan" aria-expanded="false" aria-controls="neurips2021_nandan">
-                                [NeurIPS, 2021] BEIR: A Heterogeneous Benchmark for Zero-shot Evaluation of Information Retrieval Models.
+                                [<b>NeurIPS</b>, 2021] BEIR: A Heterogeneous Benchmark for Zero-shot Evaluation of Information Retrieval Models.
                             </button>
                             </h2>
                             <div id="neurips2021_nandan" class="accordion-collapse collapse" aria-labelledby="neurips2021_nandan_header" data-bs-parent="#accordion_funded_by_square">
@@ -85,7 +85,7 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="NLPConv2021_Leo_header">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#NLPConv2021_Leo" aria-expanded="false" aria-controls="NLPConv2021_Leo">
-                                [NLP for Conversational AI Workshop@EMNLP, 2021] Investigating Pretrained Language Models for Graph-to-Text Generation.
+                                [<b>NLP for Conversational AI Workshop@EMNLP</b>, 2021] Investigating Pretrained Language Models for Graph-to-Text Generation.
                             </button>
                             </h2>
                             <div id="NLPConv2021_Leo" class="accordion-collapse collapse" aria-labelledby="NLPConv2021_Leo_header" data-bs-parent="#accordion_funded_by_square">
@@ -99,7 +99,7 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="emnlp2021_leo_header">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#emnlp2021_leo" aria-expanded="false" aria-controls="emnlp2021_leo">
-                                [EMNLP, 2021] Smelting Gold and Silver for Improved Multilingual AMR-to-Text Generation.
+                                [<b>EMNLP</b>, 2021] Smelting Gold and Silver for Improved Multilingual AMR-to-Text Generation.
                             </button>
                             </h2>
                             <div id="emnlp2021_leo" class="accordion-collapse collapse" aria-labelledby="emnlp2021_leo_header" data-bs-parent="#accordion_funded_by_square">
@@ -115,7 +115,7 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="emnlp2021_kexin_header">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#emnlp2021_kexin" aria-expanded="false" aria-controls="emnlp2021_kexin">
-                                [Findings of EMNLP, 2021] TSDAE: Using Transformer-based Sequential Denoising Auto-Encoder for Unsupervised Sentence Embedding Learning.
+                                [<b>Findings of EMNLP</b>, 2021] TSDAE: Using Transformer-based Sequential Denoising Auto-Encoder for Unsupervised Sentence Embedding Learning.
                             </button>
                             </h2>
                             <div id="emnlp2021_kexin" class="accordion-collapse collapse" aria-labelledby="emnlp2021_kexin_header" data-bs-parent="#accordion_funded_by_square">
@@ -129,7 +129,7 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="arxiv2021_haritz_header">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#arxiv2021_haritz" aria-expanded="false" aria-controls="arxiv2021_haritz">
-                                [ArXiv, 2021] MetaQA: Combining Expert Agents for Multi-Skill Question Answering
+                                [<b>ArXiv</b>, 2021] MetaQA: Combining Expert Agents for Multi-Skill Question Answering
                             </button>
                             </h2>
                             <div id="arxiv2021_haritz" class="accordion-collapse collapse" aria-labelledby="arxiv2021_haritz_header" data-bs-parent="#accordion_funded_by_square">
@@ -144,7 +144,7 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="arxiv2021_gregor_header">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#arxiv2021_gregor" aria-expanded="false" aria-controls="arxiv2021_gregor">
-                                [ArXiv, 2021] TWEAC: Transformer with Extendable QA Agent Classifiers
+                                [<b>ArXiv</b>, 2021] TWEAC: Transformer with Extendable QA Agent Classifiers
                             </button>
                             </h2>
                             <div id="arxiv2021_gregor" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordion_funded_by_square">
