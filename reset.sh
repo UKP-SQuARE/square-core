bfbff91c60a253d436712a641c7842ecaa7ad998
kwang2049 <kexin.wang.2049@gmail.com>
2022-10-28 01:35:13 +0200

    Set up private_key.pem to skip auth; docker volume rm specific volumes

--- reset.sh
+++ reset.sh
@@ -3,10 +3,18 @@ if [ -f "./docker-compose.yaml" ]; then
 	docker-compose down
 fi
 rm .env ./**/.env
-docker volume prune -f
+for docker_volume in "square-core_traefik-public-certificates" "square-core_model_configs" "square-core_onnx_models" "square-core_mongo-data" "square-core_db-data" "square-core_datastore-api-es" "square-core_mongo-dumps" "square-core_square-redis-data"
+do
+	docker volume rm $docker_volume
+done
 
 # Set mode to local:
 sed -i -e "s/environment: prod/environment: local/g" ./config.yaml
+
+# Generate local-deployment key
+mkdir -p local_deploy
+export SQUARE_PRIVATE_KEY_FILE="${PWD}/local_deploy/private_key.pem"
+square_pk
 
 # Set init client secrets:
 sed -e "s/%%CLIENT_SECRET%%/skill-manager/g" skill-manager/.env.template > skill-manager/.env
