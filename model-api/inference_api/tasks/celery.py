90b5008efee8590735aedd9251c6d80bee7265e8
Rachneet <rachneetsachdeva@yahoo.com>
2022-11-14 10:55:06 +0100

    feat: local deployment of models

--- model-api/inference_api/tasks/celery.py
+++ model-api/inference_api/tasks/celery.py
@@ -9,6 +9,7 @@ logger = logging.getLogger(__name__)
 # use env vars
 rabbitmq_user = os.getenv("RABBITMQ_DEFAULT_USER", "ukp")
 rabbitmq_password = os.getenv("RABBITMQ_DEFAULT_PASS", "secret")
+rabbitmq_host = os.getenv("RABBITMQ_HOST", "rabbit")
 
 redis_user = os.getenv("REDIS_USER", "ukp")
 redis_password = os.getenv("REDIS_PASSWORD", "secret")
@@ -17,7 +18,7 @@ redis_host = os.getenv("REDIS_HOST", "redis")
 app = Celery(
     "tasks",
     backend=f"redis://{redis_user}:{redis_password}@{redis_host}:6379",
-    broker=f"amqp://{rabbitmq_user}:{rabbitmq_password}@rabbit:5672//",
+    broker=f"amqp://{rabbitmq_user}:{rabbitmq_password}@{rabbitmq_host}:5672//",
     include=["tasks.tasks"],
 )
 
