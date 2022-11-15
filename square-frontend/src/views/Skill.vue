cb648da2651e62b9f7f2c3c7fc28a3f14a9ad6cf
Benedikt Stuhrmann <benedikt.stuhrmann@gmail.com>
2022-11-15 14:09:53 +0100

    Added missing import for vue-multiselect.01

--- square-frontend/src/views/Skill.vue
+++ square-frontend/src/views/Skill.vue
@@ -143,6 +143,7 @@ import Vue from 'vue'
 import Alert from '@/components/Alert.vue'
 import Card from '@/components/Card.vue'
 import Status from '@/components/Status.vue'
+import Multiselect from 'vue-multiselect'
 import { getSkill, getSkillTypes, getDataSets } from '@/api'
 
 export default Vue.component('edit-skill', {
