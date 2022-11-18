cf81ea89414b67e9d6cde4c2330033c864f7f3cf
Haritz Puerto <haritzpuerto94@gmail.com>
2022-11-18 20:47:42 +0100

    explain -> behavioral_tests

--- square-frontend/src/components/NavBar.vue
+++ square-frontend/src/components/NavBar.vue
@@ -17,7 +17,7 @@
                 <router-link class="nav-link" to="/qa" exact-active-class="active">QA</router-link>
               </li>
               <li class="nav-item">
-                <router-link class="nav-link" to="/explain" exact-active-class="active">Behavioral Tests</router-link>
+                <router-link class="nav-link" to="/behavioral_tests" exact-active-class="active">Behavioral Tests</router-link>
               </li>
               <li class="nav-item">
                 <router-link class="nav-link" to="/publications" exact-active-class="active">Publications</router-link>
