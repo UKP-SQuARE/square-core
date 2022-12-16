44d9a40134f8e3048b2db4853a437aff3b6de48a
Haritz Puerto <haritzpuerto94@gmail.com>
2022-12-16 16:58:52 +0100

    added as a comment the selection of top k preds from tweac

--- skills/tweac/skill.py
+++ skills/tweac/skill.py
@@ -1,11 +1,13 @@
 import logging
 import os
-
 import requests
+import numpy as np
+
 from square_auth.client_credentials import ClientCredentials
 from square_datastore_client import SQuAREDatastoreClient
 from square_model_client import SQuAREModelClient
 from square_skill_api.models import QueryOutput, QueryRequest
+
 
 logger = logging.getLogger(__name__)
 
@@ -49,9 +51,35 @@ async def _call_tweac(request):
     #                   'adversarial': {}
     #                   }
     logger.info("TWEAC response: {}".format(model_response))
+    # "logits": [
+    #     [
+    #       0.9998825788497925,
+    #       5.138603683008114e-06,
+    #       7.102704330463894e-06,
+    #       1.022551441565156e-05,
+    #       4.9858945203595795e-06,
+    #       4.930585419060662e-06,
+    #       4.98797771797399e-06,
+    #       3.965129053540295e-06,
+    #       4.125905888940906e-06,
+    #       3.048108374059666e-05,
+    #       7.981140697665978e-06,
+    #       6.66554251438356e-06,
+    #       6.2153631006367505e-06,
+    #       4.478948540054262e-06,
+    #       9.486773706157692e-06,
+    #       6.597141236852622e-06
+    #     ]
+    #   ]
+    # get top 1 prediction
     raw_pred = model_response["labels"][0]
     logger.info("Raw prediction: {}".format(raw_pred))
     dataset_name = model_response["id2label"][raw_pred]
+    # get top k predictions
+    # raw_pred = model_response["logits"][0]
+    # top_k = len(raw_pred) - 1
+    # ind = np.argpartition(np.array(raw_pred), top_k)[-top_k:][::-1]
+    # list_predicted_datasets = [model_response["id2label"][i] for i in ind]
     logger.info("TWEAC prediction: {}".format(dataset_name))
     return dataset_name
 
