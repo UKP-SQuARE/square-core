fa6b38281f8e4efdf064135eda9facd8faafc2ae
Benedikt Stuhrmann <benedikt.stuhrmann@gmail.com>
2022-12-19 15:42:22 +0100

    Added empty evaluation- and leaderboard-page.

---
+++ frontend/src/views/Evaluation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>This is the Place to start evaluations</div>
+    <router-link class='btn btn-primary' to='/leaderboard' exact-active-class='active'>Go here to compare results!</router-link>
+  </div>
+</template>
+
+
+<script>
+import Vue from 'vue'
+
+export default Vue.component('do-evaluation', {
+   data() {
+    return {}
+  },
+})
+</script>
---
+++ frontend/src/views/Leaderboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>This is the Leaderboard</div>
+    <router-link class='btn btn-primary' to='/evaluation' exact-active-class='active'>Evaluate your skills now!</router-link>
+  </div>
+</template>
+
+
+<script>
+import Vue from 'vue'
+
+export default Vue.component('show-leaderboard', {
+   data() {
+    return {}
+  },
+})
+</script>
