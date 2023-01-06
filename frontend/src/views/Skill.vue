2729fcd6a88cf4848888685458ca0a6b695d900e
Haritz Puerto <haritzpuerto94@gmail.com>
2023-01-06 11:20:40 +0100

    added metaskill icon

--- frontend/src/views/Skill.vue
+++ frontend/src/views/Skill.vue
@@ -85,17 +85,18 @@
           <div v-if="url == 'Externally hosted'">
             <div class="row">
               <div class="col-6 mt-3">
-              <input v-model="extern_url" type="url" class="form-control form-control-sm" id="url" placeholder="http://...">
-              <small class="text-muted">URL to the hosted skill (<span class="text-info">scheme</span>://<span
-                  class="text-info">host</span>:<span class="text-info">port</span>/<span
-                  class="text-info">base_path</span>)</small>
+                <input v-model="extern_url" type="url" class="form-control form-control-sm" id="url"
+                  placeholder="http://...">
+                <small class="text-muted">URL to the hosted skill (<span class="text-info">scheme</span>://<span
+                    class="text-info">host</span>:<span class="text-info">port</span>/<span
+                    class="text-info">base_path</span>)</small>
+              </div>
+
+              <div class="col-1 mt-3">
+                <Status :url="extern_url" />
+              </div>
             </div>
 
-            <div class="col-1 mt-3">
-              <Status :url="extern_url" />
-            </div>
-            </div>
-            
           </div>
         </div>
       </div>
@@ -129,7 +130,12 @@
             <input v-model="skill.meta_skill" v-bind:value="skill.meta_skill" class="form-check-input" type="checkbox"
               role="switch" id="metaskill">
             <label class="form-check-label d-inline-flex align-items-center" for="metaskill">
-              Meta-Skill? &nbsp;
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-boxes"
+                viewBox="0 0 16 16">
+                <path
+                  d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434L7.752.066ZM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567L4.25 7.504ZM7.5 9.933l-2.75 1.571v3.134l2.75-1.571V9.933Zm1 3.134 2.75 1.571v-3.134L8.5 9.933v3.134Zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567-2.742 1.567Zm2.242-2.433V3.504L8.5 5.076V8.21l2.75-1.572ZM7.5 8.21V5.076L4.75 3.504v3.134L7.5 8.21ZM5.258 2.643 8 4.21l2.742-1.567L8 1.076 5.258 2.643ZM15 9.933l-2.75 1.571v3.134L15 13.067V9.933ZM3.75 14.638v-3.134L1 9.933v3.134l2.75 1.571Z" />
+              </svg>
+              &nbsp; Meta-Skill &nbsp;
               <svg
                 content="Meta-Skills are Skills that combine two or more Skills. Currently, TWEAC (routing), Meta-QA (fusion of predictions), and average of adapter werights are supported."
                 v-tippy xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
