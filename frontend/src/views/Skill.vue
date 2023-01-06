804a3d719cad6569e563be1e6b940056cadd4798
Haritz Puerto <haritzpuerto94@gmail.com>
2023-01-06 10:36:05 +0100

    added meta-skill button

--- frontend/src/views/Skill.vue
+++ frontend/src/views/Skill.vue
@@ -83,15 +83,62 @@
           <small class="text-muted">URL to the hosted skill</small>
 
           <div v-if="url == 'Externally hosted'">
-            <div class="col-5 mt-3"></div>
-            <input v-model="extern_url" type="url" class="form-control" id="url" placeholder="http://...">
-            <small class="text-muted">URL to the hosted skill (<span class="text-info">scheme</span>://<span
-                class="text-info">host</span>:<span class="text-info">port</span>/<span
-                class="text-info">base_path</span>)</small>
+            <div class="row">
+              <div class="col-6 mt-3">
+              <input v-model="extern_url" type="url" class="form-control form-control-sm" id="url" placeholder="http://...">
+              <small class="text-muted">URL to the hosted skill (<span class="text-info">scheme</span>://<span
+                  class="text-info">host</span>:<span class="text-info">port</span>/<span
+                  class="text-info">base_path</span>)</small>
+            </div>
 
-            <div class="col-1 mt-5">
+            <div class="col-1 mt-3">
               <Status :url="extern_url" />
             </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+
+      <div class="row mt-2">
+        <div class="col-md-6">
+          <div class="form-check form-switch">
+            <input v-model="skill.published" v-bind:value="skill.published" class="form-check-input" type="checkbox"
+              role="switch" id="published">
+            <label class="form-check-label d-inline-flex align-items-center" for="published">
+              <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-globe"
+                viewBox="0 0 16 16">
+                <path
+                  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z" />
+              </svg>
+              &nbsp; Public Skill &nbsp;
+              <svg
+                content="Select this if you want your skill to be publicly available for any user. If unselected, the skill will only be available to you."
+                v-tippy xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-info-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path
+                  d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+              </svg>
+            </label>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="form-check form-switch">
+            <input v-model="skill.meta_skill" v-bind:value="skill.meta_skill" class="form-check-input" type="checkbox"
+              role="switch" id="metaskill">
+            <label class="form-check-label d-inline-flex align-items-center" for="metaskill">
+              Is it a Meta-Skill? &nbsp;
+              <svg
+                content="Meta-Skills are Skills that combine two or more Skills. Currently, TWEAC (routing), Meta-QA (fusion of predictions), and average of adapter werights are supported."
+                v-tippy xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-info-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path
+                  d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+              </svg>
+            </label>
           </div>
         </div>
       </div>
@@ -251,24 +298,6 @@
         </div>
       </div>
 
-      <div class="row mt-2">
-        <div class="col-md-6">
-          <div class="form-check form-switch">
-            <input v-model="skill.published" v-bind:value="skill.published" class="form-check-input" type="checkbox"
-              role="switch" id="published">
-            <label class="form-check-label d-inline-flex align-items-center" for="published">
-              <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-globe"
-                viewBox="0 0 16 16">
-                <path
-                  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z" />
-              </svg>
-              &nbsp;Public Skill
-            </label>
-          </div>
-        </div>
-      </div>
-
-
       <div class="row">
         <div class="col mt-4">
           <h3>Provide example questions</h3>
@@ -366,6 +395,7 @@ export default Vue.component('edit-skill', {
         default_skill_args: null,
         user_id: '',
         published: true,
+        meta_skill: false,
         skill_input_examples: []
       },
       skill_args: {
@@ -595,6 +625,16 @@ export default Vue.component('edit-skill', {
     'skill_args.datastore'() {
       this.indices = []
       this.setSelectIndices()
+    },
+    'url'() {
+      if (this.url == 'http://tweac' || this.url == 'http://meta-qa') {
+        this.skill.meta_skill = true
+      } else {
+        this.skill.meta_skill = false
+      }
+    },
+    'average_adapters'() {
+      this.skill.meta_skill = this.average_adapters
     }
   },
   beforeMount() {
