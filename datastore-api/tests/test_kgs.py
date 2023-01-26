db782342a56cf75c2976c48cebb2256df3e67e87
SevenDaysDA <s.tariverdian@hotmail.de>
2023-01-26 14:53:34 +0100

    Delete old tests

--- datastore-api/tests/test_kgs.py
+++ datastore-api/tests/test_kgs.py
@@ -62,21 +62,6 @@ class TestKGs:
 
         # Check wheter description is not null
         assert all(len(name["description"]) > 2 for name in response.json().values())
-    
-    def test_wikidata_get_subgraph(self, client: TestClient, wikidata_kg_name:str):
-        # Given:
-        url = f"/datastores/kg/{wikidata_kg_name}/subgraph/query_by_node_name"
-        expected_code = 200
-
-        # When:
-        response = client.post(
-            url,
-            json = ["Barack Obama", "Bill Clinton"] )
-
-        # Then:
-        assert response.status_code == expected_code
-        assert response.json() == {'Barack Obama': ['http://www.wikidata.org/entity/Q76', 'http://www.wikidata.org/entity/Q47513588', 'http://www.wikidata.org/entity/Q61909968'], 'Bill Clinton': ['http://www.wikidata.org/entity/Q1124', 'http://www.wikidata.org/entity/Q2903164', 'http://www.wikidata.org/entity/Q47508810', 'http://www.wikidata.org/entity/Q47513276', 'http://www.wikidata.org/entity/Q47513347', 'http://www.wikidata.org/entity/Q77009656']}
-        
 
     def test_wikidata_get_edges_nids(self, client: TestClient, wikidata_kg_name:str):
         # Given:
@@ -92,3 +77,4 @@ class TestKGs:
         assert response.status_code == 200
         assert any(rel["description"].split(";")[0] == "P26" for i, rel in response.json().items())
         assert any(rel["description"].split(";")[0] == "P39" for i, rel in response.json().items())
+ 
