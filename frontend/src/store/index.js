f1e37471de28c4914bc1d12211e7df762e6b6191
root <Hao.Z@outlook.de>
2023-02-02 14:01:53 +0000

    fix: remove token

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -70,7 +70,6 @@ export default new Vuex.Store({
       state.availableDatastores = payload.datastores
 
     },
-
     setIndices(state, payload) {
       state.availableDatastores.forEach(datastore=>{
         if (datastore.name==payload.dataStoreId){
@@ -200,7 +199,7 @@ export default new Vuex.Store({
       if (state.token) {
         return {'Authorization': `Bearer ${state.token}`}
       } else {
-        return {'Authorization': `Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIyUmE2VkdrUTdSNkJ3STk5WVpBLVJVQWhaaS12TlJmcTVLam11SXVMbkVZIn0.eyJleHAiOjE2NzUzNDYxOTAsImlhdCI6MTY3NTM0NTg5MCwianRpIjoiYjM5N2QyZTAtNjE1Ny00NjcyLWFjZjMtMWM5N2FkNDZlMGYzIiwiaXNzIjoiaHR0cHM6Ly9zcXVhcmUudWtwLWxhYi5kZS9hdXRoL3JlYWxtcy9zcXVhcmUiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiYjhmZWNlMTEtYWM0Yi00ZDRhLWJiYzktMWU5OGMzNzMzOWFiIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoibW9kZWxzIiwiYWNyIjoiMSIsInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJkZWZhdWx0LXJvbGVzLXNxdWFyZSIsIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6InByb2ZpbGUgZW1haWwiLCJjbGllbnRJZCI6Im1vZGVscyIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiY2xpZW50SG9zdCI6IjEwLjE2Ny4xLjI1NCIsInByZWZlcnJlZF91c2VybmFtZSI6InNlcnZpY2UtYWNjb3VudC1tb2RlbHMiLCJjbGllbnRBZGRyZXNzIjoiMTAuMTY3LjEuMjU0In0.dxJ_ASFPmU5ojzDDAptdV2tODwqSlnrvZAU01IuTEIXVhsdEu2QED3cXjJMklMCW9xitwThhGDEDB4Cp3Lr2H8Iv-bEZ1XM5zEBfUka01ZOBv8JSZBQyCzGx42cHdhiM24jP-zvVwyhfVrL6XiuzeGdglaug9RLGzuhpsqIoT4RwYk4g34Hj3k7hSbx2ZV1J8PD6F69qTZcHrTzMgZoZkE2ctXC5V0XnAyP_XyoelvctcePcch9knIOjnCzygUoZ42yKx6pAOVuhlvQ9imZF9HeQcKhIKFKjFis1G2q1R2tchXs7S_bX9jQfAtv984cz0hHselO90r2oI6CgBYRv8w`}
+        return { }
 
       }
     }
