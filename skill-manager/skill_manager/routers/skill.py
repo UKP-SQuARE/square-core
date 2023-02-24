1a84e9da3b0df32ce669100a87c8d1a55dd9d8f5
Tim Baumgärtner <baumgaertner.t@gmail.com>
2023-02-24 12:47:23 +0100

    remove dataset endpoint

--- skill-manager/skill_manager/routers/skill.py
+++ skill-manager/skill_manager/routers/skill.py
@@ -78,42 +78,6 @@ async def get_skills(request: Request):
     return skills
 
 
-@router.get(
-    "/dataset/{hf_name}/{dataset}",
-    response_model=List[Skill],
-)
-@router.get(
-    "/dataset/{dataset}",
-    response_model=List[Skill],
-)
-async def get_skills_by_dataset(
-    request: Request, hf_name: str = None, dataset: str = None
-):
-    """Returns all the skills that were trained on the given dataset."""
-    if hf_name is not None:
-        dataset = f"{hf_name}/dataset"
-
-    user_avail_skills_query = {"published": True}
-    if has_auth_header(request):
-        payload = await get_payload_from_token(request)
-        user_id = payload["username"]
-        user_avail_skills_query = {"$or": [{"published": True}, {"user_id": user_id}]}
-
-    # make a mongo query that retrieves any skill that published is True or belongs to the user and dataset is in data_sets
-    mongo_query = {"$and": [user_avail_skills_query, {"data_sets": dataset}]}
-
-    logger.debug("Skill query: {query}".format(query=json.dumps(mongo_query)))
-    skills = mongo_client.client.skill_manager.skills.find(mongo_query)
-    skills = [Skill.from_mongo(s) for s in skills]
-
-    logger.debug(
-        "get_skills: {skills}".format(
-            skills=", ".join(["{}:{}".format(s.name, str(s.id)) for s in skills])
-        )
-    )
-    return skills
-
-
 @router.post(
     "",
     response_model=Skill,
