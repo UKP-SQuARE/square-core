e606691a149486a40cd59c843507d37bd3c404eb
Haritz Puerto <haritzpuerto94@gmail.com>
2023-03-06 23:32:10 +0100

    fix bug addSkillAgents

--- frontend/src/views/Skill.vue
+++ frontend/src/views/Skill.vue
@@ -55,7 +55,7 @@
                   d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
                 <path
                   d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z" />
-                                                                                                                                                  </svg> -->
+                                                                                                                                                    </svg> -->
               Requires context
             </label>
           </div>
@@ -248,8 +248,8 @@
               <small class="text-muted">(leave empty if not required)
                 <svg
                   content="If your Skill requires the use of a datastore (i.e., a document collection) because
-                                                                                                                                                    it is an open-domain Skill, write the name of the datastore here.
-                                                                                                                                                     eg: 'bioasq' Leave blank if unsure"
+                                                                                                                                                      it is an open-domain Skill, write the name of the datastore here.
+                                                                                                                                                       eg: 'bioasq' Leave blank if unsure"
                   v-tippy xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                   class="bi bi-info-circle" viewBox="0 0 20 20">
                   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
@@ -270,8 +270,8 @@
             <small class="text-muted">(leave empty if not required)
               <svg
                 content="If your Skill is using a datatore and you do not want to use the predefined index (i.e., bm25),
-                                                                                                                                                    write the name of the index here.
-                                                                                                                                                     eg: 'distilbert'. If you selected 'requires context', then you do not need a datastore. Leave blank if unsure"
+                                                                                                                                                      write the name of the index here.
+                                                                                                                                                       eg: 'distilbert'. If you selected 'requires context', then you do not need a datastore. Leave blank if unsure"
                 v-tippy xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                 class="bi bi-info-circle" viewBox="0 0 20 20">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
@@ -555,7 +555,7 @@ export default Vue.component('edit-skill', {
     addSkillAgents() {
       this.skill.default_skill_args['list_skills'] = []
       // filter list_skills for skills with names in list_agents
-      this.list_skills.filter(skill => this.list_agents.includes(skill.name)).forEach(skill => {
+      this.list_skills.filter(skill => this.metaqa_agents.includes(skill.name)).forEach(skill => {
         this.skill.default_skill_args['list_skills'].push(skill.id)
       })
 
