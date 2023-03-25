a5fefb49ca2025d491044b5fc18eb5c89d97f79c
Haritz Puerto <haritzpuerto94@gmail.com>
2023-03-25 13:58:46 +0100

    improve UI of nav buttons

--- frontend/src/components/Query.vue
+++ frontend/src/components/Query.vue
@@ -185,13 +185,35 @@
     </div>
     <div v-if="minSkillsSelected(1)" class="col">
       <div class="row mt-4">
-        <div class="d-grid gap-1 d-md-flex justify-content-md-center">
+        <!-- Button to go back to QA Hub -->
+        <div class="col col-6 text-end">
           <router-link to="/qa_hub" class="btn btn-outline-secondary btn-lg shadow">
-            Back to QA Hub
+            <span class="d-flex align-items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                  d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z" />
+                <path fill-rule="evenodd"
+                  d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
+              </svg>&nbsp;Back to QA Hub
+            </span>
+
           </router-link>
-          <button type="submit" class="btn btn-danger btn-lg shadow text-white" :disabled="waiting">
+        </div>
+        <!-- Button to ask question -->
+        <div class="col col-6">
+          <button type="submit" class="btn btn-danger btn-lg shadow text-white d-flex align-items-center"
+            :disabled="waiting">
             <span v-show="waiting" class="spinner-border spinner-border-sm" role="status" />
-            &nbsp;Ask your question</button>
+            &nbsp;Ask your question&nbsp;
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+              class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd"
+                d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+              <path fill-rule="evenodd"
+                d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+            </svg>
+          </button>
         </div>
       </div>
 
