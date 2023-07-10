99edaa8226f55bc4af19ef767f8ae5ae97f3adf4
muhammed-shihebi <58932402+muhammed-shihebi@users.noreply.github.com>
2023-07-10 21:45:04 +0200

    handle code sections in messages

--- frontend/src/components/MessageView.vue
+++ frontend/src/components/MessageView.vue
@@ -1,33 +1,65 @@
 <template>
-    <div :class='["message", { dark }]'>
-        <!-- bold text -->
+  <div :class="['message', { dark }]">
         <strong>{{ author }}</strong>
         <br />
-        {{text}}
+    <div v-html="markdownToHtml(text)"></div>
     </div>
 </template>
 
 <script>
+import { marked } from "marked";
+import { markedHighlight } from "marked-highlight";
+import hljs from "highlight.js";
+
+marked.use({
+  mangle: false,
+  headerIds: false,
+  ...markedHighlight({
+    langPrefix: "hljs language-",
+    highlight(code, lang) {
+        console.log(code);
+        console.log(lang);
+      const language = hljs.getLanguage(lang) ? lang : "plaintext";
+      return hljs.highlight(code, {language}).value;
+    },
+  }),
+});
 
 export default {
-name: 'MessageView',
+  name: "MessageView",
     props: [
-        'text', // Content of the message
-        'author', // Author of the message
-        'dark' // Background variant of the box
-    ]
-}
+    "text", // Content of the message
+    "author", // Author of the message
+    "dark", // Background variant of the box
+  ],
+  methods: {
+    markdownToHtml(md) {
+      return marked.parse(md);
+    },
+  },
+};
 </script>
+
+
 
 <style scoped>
 .message {
-    background: #e7e7e7;
+  background: #ebebeb;
     border-radius: 10px;
     padding: 0.77rem;
     width: fit-content;
 }
-
 .message.dark {
     background: #5fc9f8;
 }
 </style>
+
+<style>
+.hljs {
+  display: block;
+  overflow-x: auto;
+  padding: 0.5em;
+  background: #d2d2d2;
+}
+</style>
+
