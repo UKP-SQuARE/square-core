f932fca37284fbd51fbc29e52b01edd3ead8ac2d
EgorZadorin <egorzadorin04@gmail.com>
2023-12-04 16:04:23 +0100

    Engaging interface, need to improve. Next step: DB connection (LLMs, Users)

--- frontend/src/views/ChatbotHome.vue
+++ frontend/src/views/ChatbotHome.vue
@@ -9,32 +9,35 @@
               <div class="container text-start"></div>
               <div class="row">
                 <div class="col text-start">
-
-                  <!-- CHANGE THE DESCRIPTION AND PURPOSE OF THIS PART -->
-
-                  <h4>Domain
-                    <svg
-                        content="A Skill is a single domain if it was trained on a single dataset, and thus is expected to only perform well in that dataset.<br/>A Skill is a multi domain if it was trained on multiple datasets to be more general.<br/>A Skill is a Meta-Skill if it combines multiple Skills."
-                        v-tippy xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                        class="bi bi-info-circle" viewBox="0 0 16 16">
-                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                  <h4>Chatbot Rating Interface</h4>
+                  <p class="mb-1">
+                    <br>
+                    Shape the future of conversational AI! <br><br>
+                    Engage with various models <br> Provide valuable feedback <br> Earn points to unlock new features <br><br>
+                    Start your journey today to become a certified LLM expert! <br><br>
+                  </p>
+                  <div v-if="isUserLoggedIn()" class="mt-5">
+                    <h4>Available Points:</h4>
+                    <span class="badge bg-primary ms-auto" style="font-size: 1.5em;">
+                      {{ points }}
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                           class="bi bi-award-fill" viewBox="0 0 16 16">
                       <path
-                          d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                          d="m8 0 1.669.864 1.858.282.842 1.68 1.337 1.32L13.4 6l.306 1.854-1.337 1.32-.842 1.68-1.858.282L8 12l-1.669-.864-1.858-.282-.842-1.68-1.337-1.32L2.6 6l-.306-1.854 1.337-1.32.842-1.68L6.331.864z"/>
+                      <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1z"/>
                     </svg>
-                  </h4>
-
-                  <span role="button" v-on:click="addRemoveScopeFilter('single_skill')"
-                        class="btn btn-outline-primary btn-sm me-1 mb-1" id="single_skill">
-                    Single Domain</span>
-                  <span role="button" v-on:click="addRemoveScopeFilter('multi_skill')"
-                        class="btn btn-outline-primary btn-sm me-1 mb-1" id="multi_skill">
-                    Multi Domain</span>
-                  <span role="button" v-on:click="addRemoveScopeFilter('meta_skill')"
-                        class="btn btn-outline-primary btn-sm me-1 mb-1" id="meta_skill">
-                    Meta-Skill</span>
+                    </span>
+                  </div>
+                  <div v-else>
+                    <button class="btn btn-primary mt-3" @click="showLoginModal">
+                      Log in to collect points
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
+
+
             <div class="col col-md-9">
               <!-- Choose the LLM -->
               <span class="fw-bold">Available Models:</span>
@@ -56,11 +59,12 @@
               <!-- Models -->
               <div class="row row-cols-2 g-3">
                 <div class="col" v-for="model in paginatedModels" :key="model.id">
-                  <div class="card h-100" :class="{'text-muted': !isModelAvailable(model)}" style="height: 25vh !important;">
+                  <div class="card h-100" :class="{'text-muted': !isModelAvailable(model)}"
+                       style="height: 25vh !important;">
                     <div class="card-body d-flex flex-column align-items-center">
                       <h5 class="card-title text-primary">{{ model.name }}</h5>
                       <p class="text-muted text-center">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+                        {{model.description}}
                       </p>
                       <span v-if="!isModelAvailable(model)" class="mt-auto">
                         <router-link :to="{ name: 'market', query: runQueryParams }"
@@ -121,13 +125,14 @@ export default Vue.component('chatbot-hub', {
       pageCount: 0,
       waiting: false,
       availableModels: [
-        {id: 1, name: 'CommonsenseQA BERT Adapter'},
-        {id: 2, name: 'Llama'},
-        {id: 3, name: 'ChatGPT-3.5'},
-        {id: 4, name: 'ChatGPT-4.0'},
-        {id: 5, name: 'Perplexity AI'}
+        {id: 1, name: 'CommonsenseQA BERT Adapter', description: 'A model specialized in common sense question answering'},
+        {id: 2, name: 'Llama', description: 'Versatile model adept at processing natural language'},
+        {id: 3, name: 'ChatGPT-3.5', description: 'Advanced conversational agent for realistic dialogue'},
+        {id: 4, name: 'ChatGPT-4.0', description: 'State-of-the-art language model with nuanced understanding'},
+        {id: 5, name: 'Perplexity AI', description: 'AI designed for deep contextual comprehension and interaction.'},
       ],
       availableModelNamesForLoggedUser: ['CommonsenseQA BERT Adapter', 'ChatGPT-3.5'],
+      points: 1500,
     }
   },
   components: {
