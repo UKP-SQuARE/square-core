f83a45da35ba90c3d60597998c9ce7e6375adef1
Lars Stockum <lars.stockum@stud.tu-darmstadt.de>
2024-01-19 13:22:51 +0100

    changes api request to profiles

--- profile-manager/profile_manager/routers/profile.py
+++ profile-manager/profile_manager/routers/profile.py
@@ -14,27 +14,26 @@ from square_auth.auth import Auth
 from profile_manager import mongo_client
 from profile_manager.core.session_cache import SessionCache
 from profile_manager.models import Badge, Submission, Certificate
-from profile_manager.settings import ProfileManagerSettings
 
 logger = logging.getLogger(__name__)
 
-settings = ProfileManagerSettings()
-router = APIRouter(prefix="/profile")
+router = APIRouter(prefix="/profiles")
 auth = Auth()
 session_cache = SessionCache()
 
 
 @router.get("/badges", response_model=List[Badge])
 async def get_badges(request: Request):
-    badges = mongo_client.client.your_database_name.badges_collection.find({})
+    test = mongo_client.client.daspChatBotRating.Profile
+    badges = mongo_client.client.daspChatBotRating.Profile.badges.find({})
     return [Badge(**badge) for badge in badges]
 
 @router.get("/submissions", response_model=List[Submission])
 async def get_submissions(request: Request):
-    submissions = mongo_client.client.your_database_name.submissions_collection.find({})
+    submissions = mongo_client.client.daspChatBotRating.Profile.submissions.find({})
     return [Submission(**submission) for submission in submissions]
 
 @router.get("/certificates", response_model=List[Certificate])
 async def get_certificates(request: Request):
-    certificates = mongo_client.client.your_database_name.certificates_collection.find({})
+    certificates = mongo_client.client.daspChatBotRating.Profile.certificates.find({})
     return [Certificate(**certificate) for certificate in certificates]
